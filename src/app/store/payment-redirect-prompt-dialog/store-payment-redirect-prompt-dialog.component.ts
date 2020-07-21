9c3852fb3a3a203c13fba148cde3f0ea1e908bc7
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-07-21 17:53:09 -0400

    split checkout logic into methods

--- src/app/store/payment-redirect-prompt-dialog/store-payment-redirect-prompt-dialog.component.ts
+++ src/app/store/payment-redirect-prompt-dialog/store-payment-redirect-prompt-dialog.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { StoreService } from '../store.service';
-import { HttpClient } from '@angular/common/http';
-import { concatMap } from 'rxjs/operators';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { concatMap, map } from 'rxjs/operators';
 import { AppUserService } from 'src/app/app-user.service';
 import { DATA_INJECTION_TOKENS } from 'src/data/services/data-injection-tokens';
-import { Observable, BehaviorSubject, Subject } from 'rxjs';
+import { Observable, BehaviorSubject, Subject, of } from 'rxjs';
 import { CompositeEntityDataIService } from 'src/data/services/composite-entity.data.iservice';
 import { Sell } from 'src/data/models/entities/Sell';
 import { SellDetail } from 'src/data/models/entities/SellDetail';
@@ -28,32 +28,37 @@ export class StorePaymentRedirectPromptDialogComponent
     @Inject(DATA_INJECTION_TOKENS.sales) protected saleDataService: CompositeEntityDataIService<Sell, SellDetail>
   ) { }
 
-  ngOnInit(): void {
-    this.fetchSellData();
+  protected parseFormData(subtotal: number): FormData {
+    const total = String(Math.round(subtotal * 1.19));
+    const sessionId = String(this.appUserService.getCurrentSession().id);
+    const formData = new FormData();
+    formData.append('tr_amount', total);
+    formData.append('tr_session', sessionId);
+    formData.append('tr_id', '1');
+    return formData;
   }
 
-  public fetchSellData(): void {
+  protected fetchWebpayHTMLForm(data: FormData): Observable<string> {
+    return this.httpClient.post(
+      'https://webpay-test.benjaminlamadrid.cl/',
+      data,
+      { responseType: 'text' }
+    );
+  }
+
+  protected loadPaymentRedirectForm(): void {
     this.service.sellSubtotalValue$.pipe(
-      concatMap(
-        subtotal => {
-          const total = Math.round(subtotal*1.19);
-          const sessionId = this.appUserService.getCurrentSession().id;
-          return this.httpClient.post<string>(
-            'http://newbazaarwebpayphp-env.eba-2ybt2gv3.sa-east-1.elasticbeanstalk.com/',
-            {
-              tr_amount: total,
-              tr_session: sessionId,
-              tr_id: 1
-            }
-          );
-        }
-      )
+      map((subtotal) => this.parseFormData(subtotal)),
+      concatMap((data) => this.fetchWebpayHTMLForm(data))
     ).subscribe(
-      s => {
-        document.getElementById('store-payment-external-content-wrapper').innerHTML = s;
+      (html: string) => {
+        document.getElementById('store-payment-external-content-wrapper').innerHTML = html;
         this.loadingSource.next(false);
       }
     )
   }
 
+  ngOnInit(): void {
+    this.loadPaymentRedirectForm();
+  }
 }
