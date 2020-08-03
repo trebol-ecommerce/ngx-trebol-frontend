275e2d144c607535aee5b3cb118241af3660961b
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-08-03 16:17:23 -0400

    add mock company data

--- src/data/services/local-memory/shared.local-memory-data.service.ts
+++ src/data/services/local-memory/shared.local-memory-data.service.ts
@@ -9,8 +9,8 @@ import { SellType } from 'src/data/models/entities/SellType';
 import { SharedDataIService } from '../shared.data.iservice';
 
 export const MOCK_COMPANY_DETAILS: CompanyDetails = {
-  name: 'Nombre de la compañía',
-  description: 'Descripción de la compañía',
+  name: 'Importaciones NBazaar',
+  description: 'Somos una pequeña empresa de importación de ropa y calzado. Llevamos más de 4 años en el mercado, conectando la manufactura fuera del país con el retail nacional.',
   bannerImageURL: 'assets/img/mikael-frivold-vJ1Pf0d-0HY-unsplash.jpg',
   logoImageURL: 'assets/img/logo.png'
 };
