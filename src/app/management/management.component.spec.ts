dff58c2d963c723c6e2bbb9e1b27ae0178c2af58
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-08-08 19:15:05 -0400

    fixes management component test

--- src/app/management/management.component.spec.ts
+++ src/app/management/management.component.spec.ts
@@ -2,15 +2,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LOCAL_MEMORY_DATA_PROVIDERS } from 'src/data/services/local-memory/local-memory-data.module';
 import { ManagementComponent } from './management.component';
 import { ManagementService } from './management.service';
+import { of } from 'rxjs';
 
 describe('ManagementComponent', () => {
   let component: ManagementComponent;
   let fixture: ComponentFixture<ManagementComponent>;
+  let service: Partial<ManagementService>;
 
   beforeEach(async(() => {
+    service = {
+      isSidenavOpen$: of(true)
+    }
     TestBed.configureTestingModule({
       declarations: [ ManagementComponent ],
-      providers: [ ManagementService, ...LOCAL_MEMORY_DATA_PROVIDERS ]
+      providers: [
+        { provide: ManagementService, useValue: service },
+        ...LOCAL_MEMORY_DATA_PROVIDERS
+      ]
     })
     .compileComponents();
   }));
