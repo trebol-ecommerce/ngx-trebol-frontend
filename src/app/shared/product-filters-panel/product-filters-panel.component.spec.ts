d30e03dd18ae1f996d0e1207b3f2dac6509d75f9
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-08-09 23:15:46 -0400

    fix product filters test

--- src/app/shared/product-filters-panel/product-filters-panel.component.spec.ts
+++ src/app/shared/product-filters-panel/product-filters-panel.component.spec.ts
@@ -1,14 +1,28 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductFiltersPanelComponent } from './product-filters-panel.component';
+import { ProductFiltersPanelService } from './product-filters-panel.service';
+import { SharedModule } from '../shared.module';
+import { of } from 'rxjs';
 
 describe('ProductFiltersPanelComponent', () => {
   let component: ProductFiltersPanelComponent;
   let fixture: ComponentFixture<ProductFiltersPanelComponent>;
+  let service: Partial<ProductFiltersPanelService>;
 
   beforeEach(async(() => {
+    service = {
+      getProductTypesFromFamilyId(i) { return of([]); }
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ ProductFiltersPanelComponent ]
+      imports: [
+        SharedModule
+      ],
+      declarations: [ ProductFiltersPanelComponent ],
+      providers: [
+        { provide: ProductFiltersPanelService, useValue: service }
+      ]
     })
     .compileComponents();
   }));
