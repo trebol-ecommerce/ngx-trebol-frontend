367be31dbee7e8094fea09e39960c88adf1c0dd1
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-08-09 22:50:21 -0400

    minor lint

--- src/app/store/routes/catalog/product-card/store-catalog-product-card.component.ts
+++ src/app/store/routes/catalog/product-card/store-catalog-product-card.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { StoreCartService } from 'src/app/store/store-cart.service';
-import { StoreCatalogService } from '../store-catalog.service';
 import { Product } from 'src/data/models/entities/Product';
+import { StoreCatalogService } from '../store-catalog.service';
 
 @Component({
   selector: 'app-store-catalog-product-card',
   templateUrl: './store-catalog-product-card.component.html',
   styleUrls: ['./store-catalog-product-card.component.css']
 })
-export class StoreCatalogProductCardComponent implements OnInit {
+export class StoreCatalogProductCardComponent {
 
   @Input() public product: Product;
 
@@ -16,10 +16,6 @@ export class StoreCatalogProductCardComponent implements OnInit {
     protected cartService: StoreCartService,
     protected catalogService: StoreCatalogService
   ) { }
-
-  ngOnInit(): void {
-  }
-
 
   public onClickAddProduct(): void {
     this.cartService.addProductToCart(this.product);
