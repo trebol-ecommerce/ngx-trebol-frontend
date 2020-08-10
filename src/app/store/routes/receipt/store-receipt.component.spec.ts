7f93db38c3081554ab54c1f458f8ef1617c5a924
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-08-09 22:35:58 -0400

    fix store receipt test

--- src/app/store/routes/receipt/store-receipt.component.spec.ts
+++ src/app/store/routes/receipt/store-receipt.component.spec.ts
@@ -1,16 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { Sell } from 'src/data/models/entities/Sell';
 import { StoreReceiptComponent } from './store-receipt.component';
 import { StoreReceiptService } from './store-receipt.service';
 
 describe('StoreReceiptComponent', () => {
   let component: StoreReceiptComponent;
   let fixture: ComponentFixture<StoreReceiptComponent>;
+  let service: Partial<StoreReceiptService>;
 
   beforeEach(async(() => {
+    service = {
+      sell$: of(new Sell()),
+      fetchSell(i) {}
+    };
+
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [ StoreReceiptComponent ],
-      providers: [ StoreReceiptService ]
+      providers: [
+        { provide: StoreReceiptService, useValue: service }
+      ]
     })
     .compileComponents();
   }));
