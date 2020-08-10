a8c6bf0b712933803c46bc8d8d5d78d66472c16f
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-08-09 22:45:17 -0400

    fix profile edit service test

--- src/app/shared/edit-profile-form-dialog/edit-profile-form.service.spec.ts
+++ src/app/shared/edit-profile-form-dialog/edit-profile-form.service.spec.ts
@@ -1,12 +1,24 @@
 import { TestBed } from '@angular/core/testing';
-
+import { LOCAL_MEMORY_DATA_PROVIDERS } from 'src/data/services/local-memory/local-memory-data.module';
 import { EditProfileFormService } from './edit-profile-form.service';
+import { AppUserService } from 'src/app/app-user.service';
 
 describe('EditProfileFormService', () => {
   let service: EditProfileFormService;
+  let userService: Partial<AppUserService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    userService = {
+      getCurrentSession() { return null; }
+    };
+
+    TestBed.configureTestingModule({
+      providers: [
+        EditProfileFormService,
+        { provide: AppUserService, useValue: userService },
+        ...LOCAL_MEMORY_DATA_PROVIDERS
+      ]
+    });
     service = TestBed.inject(EditProfileFormService);
   });
 
