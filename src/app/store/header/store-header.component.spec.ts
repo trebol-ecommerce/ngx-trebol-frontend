e8ba267559e4d610fdb8d775a29ac2a2f82baa76
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-08-09 22:41:48 -0400

    fix store header test

--- src/app/store/header/store-header.component.spec.ts
+++ src/app/store/header/store-header.component.spec.ts
@@ -1,14 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppUserService } from 'src/app/app-user.service';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { StoreCartService } from '../store-cart.service';
 import { StoreHeaderComponent } from './store-header.component';
+import { of } from 'rxjs';
 
 describe('StoreHeaderComponent', () => {
   let component: StoreHeaderComponent;
   let fixture: ComponentFixture<StoreHeaderComponent>;
+  let cartService: Partial<StoreCartService>;
+  let userService: Partial<AppUserService>;
 
   beforeEach(async(() => {
+    cartService = {
+      sellDetails$: of([]),
+      itemQuantity$: of(0),
+      sellSubtotalValue$: of(0)
+    };
+    userService = {
+      getCurrentSession() { return null; },
+      closeCurrentSession() {},
+      sessionChanges$: of(null),
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ StoreHeaderComponent ]
+      imports: [
+        RouterTestingModule,
+        SharedModule
+      ],
+      declarations: [ StoreHeaderComponent ],
+      providers: [
+        { provide: StoreCartService, useValue: cartService },
+        { provide: AppUserService, useValue: userService }
+      ]
     })
     .compileComponents();
   }));
