f05ae9b32e0ec4c146487f816ec0f1be7aa5526d
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-07 16:57:14 -0300

    implement provider data service

--- src/data/services/http/providers.http-data.service.ts
+++ src/data/services/http/providers.http-data.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { retry } from 'rxjs/operators';
 import { Provider } from 'src/data/models/entities/Provider';
 import { HttpService } from 'src/data/services/http/http.abstract-service';
 import { EntityDataIService } from '../entity.data.iservice';
@@ -16,27 +15,36 @@ export class ProvidersHttpDataService
   ) {
     super();
   }
-  readById(id: string | number): Observable<Provider> {
-    throw new Error('Method not implemented.');
+
+  public create(provider: Provider): Observable<number> {
+    return this.http.post<number>(
+      this.baseURI + '/provider',
+      provider
+    );
   }
-  readFiltered(f: any): Observable<Provider[]> {
-    throw new Error('Method not implemented.');
-  }
-  update(emp: Provider, id: string | number): Observable<number> {
-    throw new Error('Method not implemented.');
+
+  public readById(providerId: number): Observable<Provider> {
+    return this.http.get<Provider>(
+      this.baseURI + `/provider/${providerId}`
+    );
   }
 
   public readAll(): Observable<Provider[]> {
     return this.http.get<Provider[]>(
       this.baseURI + '/providers'
-    ).pipe(
-      retry(2)
     );
   }
 
-  public create(provider: Provider): Observable<number> {
-    return this.http.post<number>(
-      this.baseURI + '/provider',
+  public readFiltered(filters: any): Observable<Provider[]> {
+    return this.http.get<Provider[]>(
+      this.baseURI + '/providers',
+      this.httpParamsOf(filters)
+    );
+  }
+
+  public update(provider: Provider, providerId: string | number): Observable<number> {
+    return this.http.put<number>(
+      this.baseURI + `/provider/${providerId}`,
       provider
     );
   }
