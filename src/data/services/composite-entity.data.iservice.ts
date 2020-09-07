c2817df1facae3ecaa91b426ad5f9750704bea2b
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-07 16:52:27 -0300

    more appropiate type for interface

--- src/data/services/composite-entity.data.iservice.ts
+++ src/data/services/composite-entity.data.iservice.ts
@@ -1,7 +1,8 @@
 import { Observable } from 'rxjs';
 import { EntityDataIService } from './entity.data.iservice';
+import { AbstractEntity } from '../models/AbstractEntity';
 
-export interface CompositeEntityDataIService<T, X>
+export interface CompositeEntityDataIService<T extends AbstractEntity, X extends AbstractEntity>
   extends EntityDataIService<T> {
 
   readDetailsById(id: number): Observable<X[]>;
