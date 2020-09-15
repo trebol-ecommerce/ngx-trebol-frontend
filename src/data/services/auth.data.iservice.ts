42a5191a3d4c0ab277b5d9454410c011cb0ec7a8
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-15 20:51:10 -0300

    change auth data service interface methods

--- src/data/services/auth.data.iservice.ts
+++ src/data/services/auth.data.iservice.ts
@@ -1,9 +1,8 @@
 import { Observable } from 'rxjs';
-import { Session } from 'src/data/models/entities/Session';
 
 export interface SessionDataIService {
 
-  login(details: any): Observable<Session>;
-  validate(ssn: Partial<Session>): Observable<boolean>;
-  logout(ssn: Partial<Session>): Observable<boolean>;
+  login(details: any): Observable<boolean>;
+  validate(): Observable<boolean>;
+  logout(): Observable<boolean>;
 }
