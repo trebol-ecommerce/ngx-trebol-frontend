51c22931be92dfd36cdebf22bd5618f5ae4af1e0
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-16 20:01:33 -0300

    update auth service interface methods

--- src/app/auth/auth.iservice.ts
+++ src/app/auth/auth.iservice.ts
@@ -1,10 +1,14 @@
 import { Observable } from 'rxjs';
+import { Person } from '../data/models/entities/Person';
+import { User } from '../data/models/entities/User';
 
 export interface AuthenticationIService {
 
-  guestLogin(details: any): Observable<boolean>;
-  register(details: any): Observable<boolean>;
+  guestLogin(details: Person): Observable<boolean>;
+  register(details: Partial<User>): Observable<boolean>;
   login(details: any): Observable<boolean>;
   validate(): Observable<boolean>;
+  getProfile(): Observable<Person>;
+  updateProfile(details: Person): Observable<boolean>;
   logout(): Observable<boolean>;
 }
