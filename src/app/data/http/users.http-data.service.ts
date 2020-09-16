7dc854558940226ef087c64b5f6d86e55bed02c2
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-16 19:38:37 -0300

    format & extend user httpsvc from abstract

--- src/app/data/http/users.http-data.service.ts
+++ src/app/data/http/users.http-data.service.ts
@@ -2,12 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/data/models/entities/User';
-import { HttpService } from 'src/app/shared/http.abstract-service';
+import { EntityHttpDataService } from './entity.http-data.aservice';
 import { EntityDataIService } from '../entity.data.iservice';
 
 @Injectable()
 export class UsersHttpDataService
-  extends HttpService
+  extends EntityHttpDataService
   implements EntityDataIService<User> {
 
   constructor(
@@ -18,40 +18,40 @@ export class UsersHttpDataService
 
   public create(user: User): Observable<number> {
     return this.http.post<number>(
-      this.baseURI + '/user',
+      `${this.baseURI}/user`,
       user
     );
   }
 
   public readById(userId:  number): Observable<User> {
     return this.http.get<User>(
-      this.baseURI + `/user/${userId}`
+      `${this.baseURI}/user/${userId}`
     );
   }
 
   public readAll(): Observable<User[]> {
     return this.http.get<User[]>(
-      this.baseURI + '/users'
+      `${this.baseURI}/users`
     );
   }
 
   public readFiltered(filters: any): Observable<User[]> {
     return this.http.get<User[]>(
-      this.baseURI + '/users',
+      `${this.baseURI}/users`,
       this.httpParamsOf(filters)
     );
   }
 
   public update(user: User, userId: string | number): Observable<number> {
     return this.http.put<number>(
-      this.baseURI + `/user/${userId}`,
+      `${this.baseURI}/user/${userId}`,
       user
     );
   }
 
   public deleteById(userId: number): Observable<boolean> {
     return this.http.delete<boolean>(
-      this.baseURI + `/user/${userId}`
+      `${this.baseURI}/user/${userId}`
     );
   }
 }
