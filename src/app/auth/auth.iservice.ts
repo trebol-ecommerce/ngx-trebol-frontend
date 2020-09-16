2e844f99be37e6726b2121c40c2787916acaea29
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-16 17:36:51 -0300

    add two methods to auth service interface

--- src/app/auth/auth.iservice.ts
+++ src/app/auth/auth.iservice.ts
@@ -2,6 +2,8 @@ import { Observable } from 'rxjs';
 
 export interface AuthenticationIService {
 
+  guestLogin(details: any): Observable<boolean>;
+  register(details: any): Observable<boolean>;
   login(details: any): Observable<boolean>;
   validate(): Observable<boolean>;
   logout(): Observable<boolean>;
