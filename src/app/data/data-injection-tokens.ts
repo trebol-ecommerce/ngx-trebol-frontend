b0b26f006a8bcbf49da3cf84c2fc7dd786a8085f
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-18 00:32:21 -0300

    add data injection token definition

--- src/app/data/data-injection-tokens.ts
+++ src/app/data/data-injection-tokens.ts
@@ -2,6 +2,7 @@
  * Data service tokens for Angular dependency injection pattern
  */
 export const DATA_INJECTION_TOKENS = {
+  storeCatalog: 'StoreCatalogData',
   shared: 'SharedData',
   clients: 'ClientsData',
   employees: 'EmployeesData',
