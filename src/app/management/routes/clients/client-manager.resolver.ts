582da7a8b56a3bb0ecfa958a51b4c4700350213e
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-27 15:05:23 -0300

    add client manager data resolver

---
+++ src/app/management/routes/clients/client-manager.resolver.ts
@@ -0,0 +1,22 @@
+import { Inject, Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { DATA_INJECTION_TOKENS } from '../../../data/data-injection-tokens';
+import { EntityCrudIService } from '../../../data/entity.crud.iservice';
+import { Client } from '../../../data/models/entities/Client';
+
+@Injectable({ providedIn: 'root' })
+export class ClientManagerResolver
+  implements Resolve<Client[]> {
+  
+  constructor(
+    @Inject(DATA_INJECTION_TOKENS.clients) protected dataService: EntityCrudIService<Client>
+  ) { }
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<Client[]>|Promise<Client[]>|Client[] {
+    return this.dataService.readAll();
+  }
+}
