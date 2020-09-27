cd0b4148f43300530c7fa43926a35eb2c20b3081
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-27 18:48:06 -0300

    add new model for authorized access details

--- src/app/auth/auth.iservice.ts
+++ src/app/auth/auth.iservice.ts
@@ -1,6 +1,7 @@
 import { Observable } from 'rxjs';
 import { Person } from '../data/models/entities/Person';
 import { User } from '../data/models/entities/User';
+import { AuthorizedAccess } from '../data/models/AuthorizedAccess';
 
 export interface AuthenticationIService {
 
@@ -8,6 +9,7 @@ export interface AuthenticationIService {
   register(details: Partial<User>): Observable<boolean>;
   login(details: any): Observable<boolean>;
   validate(): Observable<boolean>;
+  getAuthorizedAccess(): Observable<AuthorizedAccess>;
   getProfile(): Observable<Person>;
   updateProfile(details: Person): Observable<boolean>;
   logout(): Observable<boolean>;
---
+++ src/app/data/models/AuthorizedAccess.ts
@@ -0,0 +1,4 @@
+export interface AuthorizedAccess {
+  routes: string[];
+  permissions: string[];
+}
