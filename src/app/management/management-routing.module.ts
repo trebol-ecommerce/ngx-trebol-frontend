e0bc0ea5957f786f655e81d7356d147ac2887a38
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-28 16:45:10 -0300

    implement access resolvers into routing module

--- src/app/management/management-routing.module.ts
+++ src/app/management/management-routing.module.ts
@@ -4,15 +4,20 @@ import { ManagementRoutingGuard } from './management-routing.guard';
 import { ManagementComponent } from './management.component';
 import { ClientManagerComponent } from './routes/clients/client-manager.component';
 import { ClientManagerItemsResolver } from './routes/clients/client-manager.items-resolver';
+import { ClientManagerAccessResolver } from './routes/clients/client-manager.access-resolver';
 import { ManagementDashboardComponent } from './routes/dashboard/management-dashboard.component';
 import { EmployeeManagerComponent } from './routes/employees/employee-manager.component';
 import { EmployeeManagerItemsResolver } from './routes/employees/employee-manager.items-resolver';
+import { EmployeeManagerAccessResolver } from './routes/employees/employee-manager.access-resolver';
 import { ProductManagerComponent } from './routes/products/product-manager.component';
 import { ProductManagerItemsResolver } from './routes/products/product-manager.items-resolver';
+import { ProductManagerAccessResolver } from './routes/products/product-manager.access-resolver';
 import { SellManagerComponent } from './routes/sales/sell-manager.component';
 import { SellManagerItemsResolver } from './routes/sales/sell-manager.items-resolver';
+import { SellManagerAccessResolver } from './routes/sales/sell-manager.access-resolver';
 import { UserManagerComponent } from './routes/users/user-manager.component';
 import { UserManagerItemsResolver } from './routes/users/user-manager.items-resolver';
+import { UserManagerAccessResolver } from './routes/users/user-manager.access-resolver';
 
 export interface ManagementChildRoute extends Route {
   data: { matIcon: string, title: string }
@@ -26,27 +31,27 @@ export const MANAGEMENT_CHILD_ROUTES: ManagementChildRoute[] = [
   {
     path: 'sales', component: SellManagerComponent, 
     data: { matIcon: 'attach_money', title: 'Ventas' }, 
-    resolve: { items: SellManagerItemsResolver } 
+    resolve: { items: SellManagerItemsResolver, access: SellManagerAccessResolver } 
   },
   {
     path: 'products', component: ProductManagerComponent, 
     data: { matIcon: 'store', title: 'Productos' }, 
-    resolve: { items: ProductManagerItemsResolver } 
+    resolve: { items: ProductManagerItemsResolver, access: ProductManagerAccessResolver } 
   },
   {
     path: 'clients', component: ClientManagerComponent, 
     data: { matIcon: 'person', title: 'Clientes' }, 
-    resolve: { items: ClientManagerItemsResolver } 
+    resolve: { items: ClientManagerItemsResolver, access: ClientManagerAccessResolver } 
   },
   {
     path: 'employees', component: EmployeeManagerComponent, 
     data: { matIcon: 'work', title: 'Empleados' }, 
-    resolve: { items: EmployeeManagerItemsResolver } 
+    resolve: { items: EmployeeManagerItemsResolver, access: EmployeeManagerAccessResolver } 
   },
   {
     path: 'users', component: UserManagerComponent, 
     data: { matIcon: 'perm_identity', title: 'Usuarios' }, 
-    resolve: { items: UserManagerItemsResolver } 
+    resolve: { items: UserManagerItemsResolver, access: UserManagerAccessResolver } 
   }
 ];
 
