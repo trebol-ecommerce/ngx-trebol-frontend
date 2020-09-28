cd415c6a4667ec7aebcbe550679f4ec4e41fbc96
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-28 16:44:59 -0300

    create access resolvers

---
+++ src/app/management/routes/users/user-manager.access-resolver.ts
@@ -0,0 +1,22 @@
+import { Inject, Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AUTH_INJECTION_TOKEN } from '../../../auth/auth.injection-token';
+import { AuthenticationIService } from '../../../auth/auth.iservice';
+import { AuthorizedAccess } from '../../../data/models/AuthorizedAccess';
+
+@Injectable({ providedIn: 'root' })
+export class UserManagerAccessResolver
+  implements Resolve<AuthorizedAccess> {
+  
+  constructor(
+    @Inject(AUTH_INJECTION_TOKEN) protected authService: AuthenticationIService
+  ) { }
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<AuthorizedAccess>|Promise<AuthorizedAccess>|AuthorizedAccess {
+    return this.authService.getResourceAuthorizedAccess('user');
+  }
+}
