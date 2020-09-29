68d93c28c2f3ef8f0bf76bc5edc5a2afb9ff3edf
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-29 00:34:41 -0300

    add catalog service methods for type & family

--- src/app/data/store.catalog.data.iservice.ts
+++ src/app/data/store.catalog.data.iservice.ts
@@ -1,9 +1,13 @@
 import { Observable } from 'rxjs';
 import { ProductFilters } from '../shared/product-filters-panel/product-filters-panel.component';
 import { Product } from './models/entities/Product';
+import { ProductFamily } from './models/entities/ProductFamily';
+import { ProductType } from './models/entities/ProductType';
 
 export interface StoreCatalogDataIService {
   readById(id: number): Observable<Product>;
   readAll(): Observable<Product[]>;
   readFiltered(filters: ProductFilters): Observable<Product[]>;
+  readProductTypes(): Observable<ProductType[]>;
+  readProductFamilies(): Observable<ProductFamily[]>;
 }
