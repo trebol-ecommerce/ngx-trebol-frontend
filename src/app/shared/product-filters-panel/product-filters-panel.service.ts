d3f9d0cb6b6af7cd7a290412de97220fb1b0fd9f
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-09-29 01:00:51 -0300

    product filters panel service uses catalog service

--- src/app/shared/product-filters-panel/product-filters-panel.service.ts
+++ src/app/shared/product-filters-panel/product-filters-panel.service.ts
@@ -2,22 +2,22 @@ import { Injectable, OnInit, Inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ProductFamily } from 'src/app/data/models/entities/ProductFamily';
 import { DATA_INJECTION_TOKENS } from 'src/app/data/data-injection-tokens';
-import { SharedDataIService } from 'src/app/data/shared.data.iservice';
 import { ProductType } from 'src/app/data/models/entities/ProductType';
+import { StoreCatalogDataIService } from 'src/app/data/store.catalog.data.iservice';
 
 @Injectable({ providedIn: 'root' })
 export class ProductFiltersPanelService {
 
   constructor(
-    @Inject(DATA_INJECTION_TOKENS.shared) protected sharedDataService: SharedDataIService,
+    @Inject(DATA_INJECTION_TOKENS.storeCatalog) protected catalogService: StoreCatalogDataIService,
   ) { }
 
   public getAllProductFamilies(): Observable<ProductFamily[]> {
-    return this.sharedDataService.readAllProductFamilies();
+    return this.catalogService.readProductFamilies();
   }
 
   public getProductTypesFromFamilyId(id: number): Observable<ProductType[]> {
-    return this.sharedDataService.readAllProductTypesByFamilyId(id);
+    return this.catalogService.readProductTypesByFamilyId(id);
   }
 
 }
