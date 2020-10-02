279a2dc95571f88dfb1735a09c266edb518eaf35
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-10-02 20:35:57 -0300

    add method to shared data service interface

--- src/app/data/shared.data.iservice.ts
+++ src/app/data/shared.data.iservice.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs';
 import { ProductFamily } from 'src/app/data/models/entities/ProductFamily';
 import { ProductType } from 'src/app/data/models/entities/ProductType';
 import { CompanyDetails } from './models/CompanyDetails';
+import { Person } from './models/entities/Person';
 import { SellType } from './models/entities/SellType';
 
 export interface SharedDataIService {
@@ -11,4 +12,5 @@ export interface SharedDataIService {
   readAllProductTypes(): Observable<ProductType[]>;
   readAllProductTypesByFamilyId(familyId: number): Observable<ProductType[]>;
   readAllSellTypes(): Observable<SellType[]>;
+  readAllPeople(): Observable<Person[]>;
 }
