420cec3c84e3cffbb1f8094adf9ffcd78ed9d007
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-10-02 20:38:43 -0300

    use shared data service to retrieve people data

--- src/app/management/routes/users/form-dialog/user-manager-form.service.ts
+++ src/app/management/routes/users/form-dialog/user-manager-form.service.ts
@@ -5,6 +5,7 @@ import { Person } from 'src/app/data/models/entities/Person';
 import { User } from 'src/app/data/models/entities/User';
 import { DATA_INJECTION_TOKENS } from 'src/app/data/data-injection-tokens';
 import { EntityCrudIService } from 'src/app/data/entity.crud.iservice';
+import { SharedDataIService } from 'src/app/data/shared.data.iservice';
 
 @Injectable()
 export class UserManagerFormService
@@ -12,12 +13,12 @@ export class UserManagerFormService
 
   constructor(
     @Inject(DATA_INJECTION_TOKENS.users) public dataService: EntityCrudIService<User>,
-    @Inject(DATA_INJECTION_TOKENS.people) protected peopleDataService: EntityCrudIService<Person>
+    @Inject(DATA_INJECTION_TOKENS.shared) protected sharedDataService: SharedDataIService
   ) {
     super();
   }
 
   public getPeople(): Observable<Person[]> {
-    return this.peopleDataService.readAll();
+    return this.sharedDataService.readAllPeople();
   }
 }
