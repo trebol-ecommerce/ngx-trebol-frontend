4bee85a4d685300095ed6c41b18a7e3d7db8c79a
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-10-05 17:34:37 -0300

    order service tokens

--- src/app/data/data-injection-tokens.ts
+++ src/app/data/data-injection-tokens.ts
@@ -3,10 +3,10 @@
  */
 export const DATA_INJECTION_TOKENS = {
   storeCatalog: 'StoreCatalogData',
+  people: 'PeopleData',
   shared: 'SharedData',
   clientsCrud: 'ClientsCrud',
   sellersCrud: 'SellersCrud',
-  people: 'PeopleData',
   productsCrud: 'ProductsCrud',
   usersCrud: 'UsersCrud',
   salesCrud: 'SalesCrud'
