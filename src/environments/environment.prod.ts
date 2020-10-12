7c4936c116ce29e77f9190ac2bb1dc18c2966ae2
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-10-12 15:29:36 -0300

    remove production environment file; it should be user-defined

--- src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  baseURI: 'https://localhost:8082/api',
-  checkoutURL: 'https://webpay-test.benjaminlamadrid.cl/',
-  sessionStorageTokenItemName: 'nm/bearer-token',
-  permissionsStorageTokenItemName: 'trebol/permissions',
-  authorizationHeaderName: 'Authorization'
-};
