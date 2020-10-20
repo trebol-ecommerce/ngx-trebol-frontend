21b6f7dee9421609ed78dc3d91d92acabfead249
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-10-20 10:03:23 -0300

    remove unused classes

--- src/app/management/sidenav/SidenavModuleItemMetadata.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2020 Benjamin
-// 
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-
-export interface SidenavModuleItemMetadata {
-  title: string;
-  materialIconName: string;
-}
