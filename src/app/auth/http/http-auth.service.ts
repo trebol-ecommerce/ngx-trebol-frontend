7fb8bfc5ec67d2f2b873f9f2edf8fc016ca794d6
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-10-21 18:56:57 -0300

    use full name

--- src/app/auth/http/http-auth.service.ts
+++ src/app/auth/http/http-auth.service.ts
@@ -1,9 +1,9 @@
-// Copyright (c) 2020 Benjamin
+// Copyright (c) 2020 Benjamin La Madrid
 // 
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
-// Copyright (c) 2020 Benjamin
+// Copyright (c) 2020 Benjamin La Madrid
 // 
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
