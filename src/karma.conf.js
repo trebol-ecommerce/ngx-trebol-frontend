630bf5f515abfb915e9cff40c68708c8bc6e6231
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-10-23 15:28:15 -0300

    configure karma for phantomjs testing

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -20,13 +20,18 @@ module.exports = function (config) {
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    // reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    // singleRun: false,
+    restartOnFileChange: true,
+    singleRun: true,
+    reporters: ['dots', 'junit'],
+    junitReporter: {
+      outputFile: 'test-results.xml'
+    }
   });
 };
