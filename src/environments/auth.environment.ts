e3125c07ce853267443aef91d3541dab7dbc0d49
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-10-25 10:35:04 -0300

    add additional environment files

---
+++ src/environments/auth.environment.ts
@@ -0,0 +1,9 @@
+// Copyright (c) 2020 Benjamin
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+export const baseURI = '';
+export const sessionStorageTokenItemName = 'trebol/bearer-token';
+export const permissionsStorageTokenItemName = 'trebol/permissions';
+export const authorizationHeaderName = 'Authorization';
