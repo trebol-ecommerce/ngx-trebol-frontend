c752dcb83b50df0113ca4be7ce656d9edd6be796
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-10-27 21:00:03 -0300

    add test for (in/de)creasing product units

--- src/app/store/store.service.spec.ts
+++ src/app/store/store.service.spec.ts
@@ -9,7 +9,7 @@ import { LocalMemoryDataModule } from 'src/app/data/local-memory/local-memory-da
 import { StoreService } from './store.service';
 import { Product } from '../data/models/entities/Product';
 import { SellDetail } from '../data/models/entities/SellDetail';
-import { delay, take } from 'rxjs/operators';
+import { take } from 'rxjs/operators';
 
 describe('StoreService', () => {
   let service: StoreService;
@@ -63,6 +63,25 @@ describe('StoreService', () => {
     service.sellDetails$.pipe(take(1)).subscribe(
       (sellDetails: SellDetail[]) => {
         expect(sellDetails.length).toBe(0);
+      }
+    );
+  });
+
+  it('should increate and decrease units of an individual item in the cart', () => {
+    service.addProductToCart(mockProduct);
+    service.addProductToCart(mockProductTwo);
+    service.increaseProductUnits(1);
+    service.increaseProductUnits(1);
+    service.sellDetails$.pipe(take(1)).subscribe(
+      (sellDetails: SellDetail[]) => {
+        expect(sellDetails[1].units).toBe(3);
+      }
+    );
+
+    service.decreaseProductUnits(1);
+    service.sellDetails$.pipe(take(1)).subscribe(
+      (sellDetails: SellDetail[]) => {
+        expect(sellDetails[1].units).toBe(2);
       }
     );
   });
