2eb54d4e4b63c2184465323f2cbb866429b9aa2b
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-11-11 23:00:22 -0300

    replace the only people api svc call

--- src/app/management/routes/users/form-dialog/user-manager-form.service.ts
+++ src/app/management/routes/users/form-dialog/user-manager-form.service.ts
@@ -10,6 +10,7 @@ import { Person } from 'src/app/models/entities/Person';
 import { User } from 'src/app/models/entities/User';
 import { API_SERVICE_INJECTION_TOKENS } from 'src/app/api/api-service-injection-tokens';
 import { EntityDataApiIService } from 'src/app/api/data-mgt/entity-data-api.iservice';
+import { SharedDataApiIService } from 'src/app/api/data-mgt/shared-data-api.iservice';
 
 @Injectable()
 export class UserManagerFormService
@@ -17,12 +18,12 @@ export class UserManagerFormService
 
   constructor(
     @Inject(API_SERVICE_INJECTION_TOKENS.usersCrud) public dataService: EntityDataApiIService<User>,
-    @Inject(API_SERVICE_INJECTION_TOKENS.people) protected peopleDataService: Partial<EntityDataApiIService<Person>>
+    @Inject(API_SERVICE_INJECTION_TOKENS.shared) protected sharedDataService: SharedDataApiIService
   ) {
     super();
   }
 
   public getPeople(): Observable<Person[]> {
-    return this.peopleDataService.readAll();
+    return this.sharedDataService.readAllPeople();
   }
 }
