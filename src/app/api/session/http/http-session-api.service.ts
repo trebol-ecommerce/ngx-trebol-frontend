f8a5d3eb528b13561fd5631cb05fc3fe62786d9a
Benjamin La Madrid <bg.lamadrid@gmail.com>
2020-11-20 11:33:31 -0300

    update base uri for session api svc

--- src/app/api/session/http/http-session-api.service.ts
+++ src/app/api/session/http/http-session-api.service.ts
@@ -23,7 +23,7 @@ export class HttpSessionApiService
   extends HttpService
   implements SessionApiIService {
 
-  protected baseURI = sessionApiURL;
+  protected baseURI = `${sessionApiURL}/session`;
   protected readonly sessionStorageTokenItemName = sessionStorageTokenItemName;
   protected readonly authorizationHeader = authorizationHeaderName;
 
