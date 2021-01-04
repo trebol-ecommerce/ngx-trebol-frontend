eb29171ed0a62993c16dc46cc5207deefc2c3619
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-01-04 08:11:21 -0300

    add tests for taking user guest prompt choice

--- src/app/store/dialogs/guest-prompt/store-guest-prompt-dialog.component.spec.ts
+++ src/app/store/dialogs/guest-prompt/store-guest-prompt-dialog.component.spec.ts
@@ -6,16 +6,28 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { MatDialogRef } from '@angular/material/dialog';
 import { StoreGuestPromptDialogComponent } from './store-guest-prompt-dialog.component';
+import { StoreGuestPromptDialogOptions } from './StoreGuestPromptDialogOptions';
+import { MatButtonModule } from '@angular/material/button';
 
 describe('StoreGuestPromptDialogComponent', () => {
   let component: StoreGuestPromptDialogComponent;
   let fixture: ComponentFixture<StoreGuestPromptDialogComponent>;
+  let mockDialogRef: Partial<MatDialogRef<StoreGuestPromptDialogComponent>>;
+  let closeSpy: jasmine.Spy;
 
   beforeEach(waitForAsync(() => {
+    mockDialogRef = {
+      close() {}
+    };
+    closeSpy = spyOn(mockDialogRef, 'close');
+
     TestBed.configureTestingModule({
+      imports: [
+        MatButtonModule
+      ],
       declarations: [ StoreGuestPromptDialogComponent ],
       providers: [
-        { provide: MatDialogRef, useValue: {} }
+        { provide: MatDialogRef, useValue: mockDialogRef }
       ]
     })
     .compileComponents();
@@ -29,5 +41,25 @@ describe('StoreGuestPromptDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should close the dialog when choosing any option', () => {
+    component.onClickOption('');
+    expect(closeSpy).toHaveBeenCalled();
+  });
+
+  it('should close the dialog with a login option result value', () => {
+    component.onClickOption('login');
+    expect(closeSpy.calls.first().args[0]).toBe(StoreGuestPromptDialogOptions.login);
+  });
+
+  it('should close the dialog with a register option result value ', () => {
+    component.onClickOption('register');
+    expect(closeSpy.calls.first().args[0]).toBe(StoreGuestPromptDialogOptions.register);
+  });
+
+  it('should close the dialog with a guest option result value', () => {
+    component.onClickOption('guest');
+    expect(closeSpy.calls.first().args[0]).toBe(StoreGuestPromptDialogOptions.guest);
   });
 });
