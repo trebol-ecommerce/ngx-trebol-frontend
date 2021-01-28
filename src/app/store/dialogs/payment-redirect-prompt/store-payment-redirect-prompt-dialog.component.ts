2916d448921dc9e9240357ca7717cfaaf397f666
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-01-28 10:16:41 -0300

    update payment redirect component to fix tests

--- src/app/store/dialogs/payment-redirect-prompt/store-payment-redirect-prompt-dialog.component.ts
+++ src/app/store/dialogs/payment-redirect-prompt/store-payment-redirect-prompt-dialog.component.ts
@@ -5,7 +5,7 @@
 
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Observable, Subject, ReplaySubject, concat, of } from 'rxjs';
-import { concatMap, map, mapTo, startWith, switchMap } from 'rxjs/operators';
+import { concatMap, map, mapTo, startWith, switchMap, catchError } from 'rxjs/operators';
 import { ExternalPaymentRedirectionData } from 'src/app/models/ExternalPaymentRedirectionData';
 import { StoreService } from 'src/app/store/store.service';
 
@@ -17,11 +17,12 @@ import { StoreService } from 'src/app/store/store.service';
 export class StorePaymentRedirectPromptDialogComponent
   implements OnInit, OnDestroy {
 
-  protected externalDataSource: Subject<ExternalPaymentRedirectionData> = new ReplaySubject(1);
+  protected externalDataSource: Subject<ExternalPaymentRedirectionData | null> = new ReplaySubject(1);
 
   public loading$: Observable<boolean>;
   public webpayURL$: Observable<string>;
   public webpayToken$: Observable<string>;
+  public error$: Observable<string | null>;
 
   constructor(
     protected storeService: StoreService
@@ -29,12 +30,16 @@ export class StorePaymentRedirectPromptDialogComponent
     this.loading$ = this.externalDataSource.asObservable().pipe(startWith(true), mapTo(false));
     this.webpayURL$ = this.externalDataSource.asObservable().pipe(map(data => data.url));
     this.webpayToken$ = this.externalDataSource.asObservable().pipe(map(data => data.token));
+    this.error$ = this.externalDataSource.asObservable().pipe(map(data => (!!data ? null : 'El servidor no pudo ser alcanzado')));
   }
 
   ngOnInit(): void {
     this.storeService.submitCart().subscribe(
       data => {
         this.externalDataSource.next(data);
+      },
+      error => {
+        this.externalDataSource.next(null);
       }
     );
   }
