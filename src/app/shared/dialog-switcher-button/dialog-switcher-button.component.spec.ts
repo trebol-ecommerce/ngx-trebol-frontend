7fcf9c101a5ebecd5f2367935b73f81575f6a603
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-01-31 15:38:15 -0300

    add test for closing/opening dialogs

--- src/app/shared/dialog-switcher-button/dialog-switcher-button.component.spec.ts
+++ src/app/shared/dialog-switcher-button/dialog-switcher-button.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DialogSwitcherButtonComponent } from './dialog-switcher-button.component';
 import { MatDialogModule, MatDialog } from '@angular/material/dialog';
+import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 describe('DialogSwitcherButtonComponent', () => {
   let component: DialogSwitcherButtonComponent;
@@ -10,6 +14,7 @@ describe('DialogSwitcherButtonComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
+        NoopAnimationsModule,
         MatDialogModule
       ],
       declarations: [ DialogSwitcherButtonComponent ]
@@ -26,5 +31,23 @@ describe('DialogSwitcherButtonComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should close a dialog and open another when calling onClick()', () => {
+    const targetDialog = ConfirmationDialogComponent;
+    const targetDialogConfig = { data: { title: 'test', message: 'test' } };
+    component.targetDialogComponent = targetDialog;
+    component.targetDialogConfig = targetDialogConfig;
+
+    const dialogRef = dialogService.open(targetDialog, targetDialogConfig);
+    component.sourceDialogRef = dialogRef;
+
+    const sourceDialogCloseSpy = spyOn(dialogRef, 'close').and.callThrough();
+    const targetDialogOpenSpy = spyOn(dialogService, 'open').and.callThrough();
+    component.onClick();
+    expect(sourceDialogCloseSpy).toHaveBeenCalled();
+    expect(targetDialogOpenSpy).toHaveBeenCalled();
+    expect(targetDialogOpenSpy.calls.first().args[0]).toBe(targetDialog);
+    expect(targetDialogOpenSpy.calls.first().args[1]).toEqual(targetDialogConfig);
   });
 });
