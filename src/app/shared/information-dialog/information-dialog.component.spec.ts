6fad87a89f0fe1f46893f6a10e181192fda83050
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-01-31 17:09:23 -0300

    provide MAT_DIALOG_DATA on tests

--- src/app/shared/information-dialog/information-dialog.component.spec.ts
+++ src/app/shared/information-dialog/information-dialog.component.spec.ts
@@ -1,17 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { InformationDialogComponent } from './information-dialog.component';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 describe('InformationDialogComponent', () => {
   let component: InformationDialogComponent;
   let fixture: ComponentFixture<InformationDialogComponent>;
+  let dialogData: any;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ InformationDialogComponent ]
+  beforeEach(waitForAsync(() => {
+    dialogData = {};
+
+    TestBed.configureTestingModule({
+      declarations: [ InformationDialogComponent ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: dialogData }
+      ]
     })
     .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(InformationDialogComponent);
