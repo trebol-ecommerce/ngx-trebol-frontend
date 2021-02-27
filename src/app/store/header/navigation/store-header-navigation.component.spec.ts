24d60418eca0995c4b2e5e78220e3865537efa29
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-02-27 00:47:37 -0300

    fix header navigation tests

--- src/app/store/header/navigation/store-header-navigation.component.spec.ts
+++ src/app/store/header/navigation/store-header-navigation.component.spec.ts
@@ -1,17 +1,38 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { CommonModule } from '@angular/common';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
+import { StoreService } from '../../store.service';
 import { StoreHeaderNavigationComponent } from './store-header-navigation.component';
 
 describe('StoreHeaderNavigationComponent', () => {
   let component: StoreHeaderNavigationComponent;
   let fixture: ComponentFixture<StoreHeaderNavigationComponent>;
+  let mockStoreService: Partial<StoreService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ StoreHeaderNavigationComponent ]
+  beforeEach(waitForAsync(() => {
+    mockStoreService = {
+      cartDetails$: of([]),
+      cartItemCount$: of(0),
+      cartSubtotalValue$: of(0)
+    };
+
+    TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        RouterTestingModule,
+        MatButtonModule,
+        MatIconModule
+      ],
+      declarations: [ StoreHeaderNavigationComponent ],
+      providers: [
+        { provide: StoreService, useValue: mockStoreService }
+      ]
     })
     .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(StoreHeaderNavigationComponent);
