cee8b77e8950ed1fe151b572310ddef2c7debb25
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-03-09 23:48:57 -0300

    introduce Image entity

---
+++ src/app/models/entities/Image.ts
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2021 Benjamin La Madrid
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+export class Image {
+  filename: string;
+  url: string;
+}
