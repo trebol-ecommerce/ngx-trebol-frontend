8227ef4e4a47fbe9b2dec0831405b61e6e0745b0
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-03-13 00:48:52 -0300

    image compliance with abstractentity

--- src/app/models/entities/Image.ts
+++ src/app/models/entities/Image.ts
@@ -1,11 +1,16 @@
-/**
+/*
  * Copyright (c) 2021 Benjamin La Madrid
  *
  * This software is released under the MIT License.
  * https://opensource.org/licenses/MIT
  */
 
-export class Image {
+import { AbstractEntity } from '../AbstractEntity';
+
+export class Image
+  extends AbstractEntity {
+
   filename: string;
   url: string;
+  get id() { return this.url; }
 }
