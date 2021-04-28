002981ea5276793fea603cc98173c26960147870
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-04-27 21:00:33 -0400

    add service to cache images

---
+++ src/app/shared/services/images.service.spec.ts
@@ -0,0 +1,57 @@
+import { TestBed } from '@angular/core/testing';
+import { EntityDataApiIService } from 'src/app/api/data/entity-data-api.iservice';
+import { Image } from 'src/app/models/entities/Image';
+import { ImagesService } from './images.service';
+import { API_SERVICE_INJECTION_TOKENS } from 'src/app/api/api-service-injection-tokens';
+import { of } from 'rxjs';
+import { take } from 'rxjs/operators';
+
+describe('ImagesService', () => {
+  let service: ImagesService;
+  let mockDataService: Partial<EntityDataApiIService<Image>>;
+
+  beforeEach(() => {
+    mockDataService = {
+      readAll() { return of([]); }
+    };
+
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: API_SERVICE_INJECTION_TOKENS.imagesCrud, useValue: mockDataService }
+      ]
+    });
+    service = TestBed.inject(ImagesService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should cache images after creation and expose an observable of its cache array', () => {
+    service.images$.pipe(
+      take(1)
+    ).subscribe(
+      payload => {
+        expect(payload).toBeTruthy();
+      }
+    );
+  });
+
+  it('should re-cache after calling fetch()', () => {
+    const exampleImageArray: Image[] = [ { url: 'fake', filename: 'example', id: 'fake' } ];
+    mockDataService = {
+      readAll() { return of(exampleImageArray); }
+    };
+    TestBed.overrideProvider(API_SERVICE_INJECTION_TOKENS.imagesCrud, { useValue: mockDataService });
+
+    service.fetch();
+    service.images$.pipe(
+      take(1)
+    ).subscribe(
+      payload => {
+        expect(payload).toEqual(exampleImageArray);
+      }
+    );
+
+  });
+});
