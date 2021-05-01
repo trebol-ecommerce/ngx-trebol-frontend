3942d39f9d587e142b571e9ebf98dcefc1413546
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-05-01 19:43:20 -0400

    implement retrieving images as option class

--- src/app/management/dialogs/images-array/images-array.service.ts
+++ src/app/management/dialogs/images-array/images-array.service.ts
@@ -1,13 +1,17 @@
-// Copyright (c) 2021 Benjamin La Madrid
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
+/**
+ * Copyright (c) 2021 Benjamin La Madrid
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
 
 import { Injectable, OnDestroy } from '@angular/core';
-import { BehaviorSubject, Observable, of, ReplaySubject } from 'rxjs';
-import { concatMap as switchMap, map } from 'rxjs/operators';
+import { BehaviorSubject, Observable, of, ReplaySubject, iif } from 'rxjs';
+import { concatMap as switchMap, map, share } from 'rxjs/operators';
 import { Image } from 'src/app/models/entities/Image';
 import { ImagesService } from 'src/app/shared/services/images.service';
+import { ImageArrayOption } from './imageArrayOption';
+import { ImagesArrayDialogData } from './ImagesArrayDialogData';
 
 @Injectable({ providedIn: 'root' })
 export class ImagesArrayService
@@ -31,6 +35,17 @@ export class ImagesArrayService
     this.filterSource.complete();
   }
 
+  fetchOptions(data?: ImagesArrayDialogData): Observable<ImageArrayOption[]> {
+    return iif(
+      () => (data?.existing?.length !== 0 &&
+              data.existing.length > 0),
+      this.convertImagesToOptionsAndProcessExistingFrom(data),
+      this.convertImagesToOptions()
+    ).pipe(
+      share()
+    );
+  }
+
   private filterCachedImages(): Observable<Image[]> {
     return this.filterSource.asObservable().pipe(
       switchMap(filter => {
@@ -44,6 +59,30 @@ export class ImagesArrayService
         }
         return this.imageDataService.images$;
       })
+    );
+  }
+
+  private convertImagesToOptions(): Observable<ImageArrayOption[]> {
+    return this.imageList$.pipe(
+      map(imageList => imageList.map(
+            image => ({ image, selected: false, disabled: false })
+      ))
+    );
+  }
+
+  private convertImagesToOptionsAndProcessExistingFrom(data: ImagesArrayDialogData): Observable<ImageArrayOption[]> {
+    return this.imageList$.pipe(
+      map(imageList => imageList.map(
+            image => {
+              const option = { image, selected: false, disabled: false };
+              if (data.existing.some(image2 =>
+                                          (image2.filename === image.filename))) {
+                option.selected = true;
+                option.disabled = true;
+              }
+              return option;
+            }
+      ))
     );
   }
 }
