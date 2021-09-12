cad9aecd3aeb1bb2e0f9299260f475fa22c9bff3
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-09-11 22:14:40 -0300

    fix lint error

--- src/environments/environment-modules.ts
+++ src/environments/environment-modules.ts
@@ -1,5 +1,5 @@
 import { LocalMemoryApiModule } from 'src/app/api/local-memory/local-memory-api.module';
-//import { HttpApiModule } from 'src/app/api/http/http-api.module';
+// import { HttpApiModule } from 'src/app/api/http/http-api.module';
 
 export const environmentModules = [
   LocalMemoryApiModule,
