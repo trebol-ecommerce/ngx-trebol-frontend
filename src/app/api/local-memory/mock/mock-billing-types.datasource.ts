29da42b7cb1c4a383d8df127308bf64d331038af
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-09-13 18:33:48 -0300

    rename billing types mock datasource

--- src/app/api/local-memory/mock/mock-billing-types.datasource.ts
+++ src/app/api/local-memory/mock/mock-billing-types.datasource.ts
@@ -1,6 +1,6 @@
 import { BillingType } from 'src/app/models/entities/BillingType';
 
-export const MOCK_SELL_TYPES: BillingType[] = [
+export const MOCK_BILLING_TYPES: BillingType[] = [
   { id: 'B', description: 'Boleta' },
   { id: 'F', description: 'Factura' }
 ];
