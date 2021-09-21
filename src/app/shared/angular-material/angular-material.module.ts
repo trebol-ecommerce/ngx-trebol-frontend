b348f850fb4f54a01c7e3a09bcc6c72421bc7ffa
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-09-21 01:19:02 -0300

    add material modules for elements to be used soon

--- src/app/shared/angular-material/angular-material.module.ts
+++ src/app/shared/angular-material/angular-material.module.ts
@@ -8,6 +8,7 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -16,9 +17,11 @@ import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatStepperModule } from '@angular/material/stepper';
 import { MatTableModule } from '@angular/material/table';
 import { MatNativeDateModule } from '@angular/material/core';
 
@@ -27,6 +30,7 @@ const MATERIAL_MODULES = [
   MatButtonModule,
   MatCardModule,
   MatDialogModule,
+  MatDividerModule,
   MatExpansionModule,
   MatFormFieldModule,
   MatIconModule,
@@ -35,9 +39,11 @@ const MATERIAL_MODULES = [
   MatMenuModule,
   MatProgressBarModule,
   MatProgressSpinnerModule,
+  MatRadioModule,
   MatSelectModule,
   MatSnackBarModule,
   MatSidenavModule,
+  MatStepperModule,
   MatTableModule
 ];
 
