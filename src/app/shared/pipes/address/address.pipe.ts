5372ae566630c8e85c000b504a6afc7ec43ac1f5
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-09-21 01:36:08 -0300

    fix address pipe

--- src/app/shared/pipes/address/address.pipe.ts
+++ src/app/shared/pipes/address/address.pipe.ts
@@ -1,15 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Address } from 'src/app/models/Address';
+import { Address } from 'src/app/models/entities/Address';
 
 @Pipe({ name: 'address' })
 export class AddressPipe implements PipeTransform {
 
   transform(value: Address, ...args: unknown[]): unknown {
     return (value.firstLine
-      + (value.secondLine? ', ' + value.secondLine : '')
+      + (value.secondLine ? ', ' + value.secondLine : '')
       + ', ' + value.city
       + ', ' + value.municipality
-      + (value.notes? ' (Comentario: ' + value.notes + ')' : ''));
+      + (value.notes ? ' (Comentario: ' + value.notes + ')' : ''));
   }
 
 }
