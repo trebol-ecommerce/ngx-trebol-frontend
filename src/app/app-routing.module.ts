dd0ab33e9681822909fb4178191ba1ef96081d7a
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-09-21 20:35:33 -0300

    ng update @angular/core@11 @angular/cli@11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,23 +10,21 @@ import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(
-      [
-        {
-          path: 'store',
-          loadChildren: () => import('./store/store.module').then(m => m.StoreModule),
-          data: { preload: true }
-        },
-        {
-          path: 'management',
-          loadChildren: () => import('./management/management.module').then(m => m.ManagementModule),
-          data: { preload: false }
-        },
-        {
-          path: '', pathMatch: 'full', redirectTo: '/store/catalog'
-        }
-      ]
-    )
+    RouterModule.forRoot([
+    {
+        path: 'store',
+        loadChildren: () => import('./store/store.module').then(m => m.StoreModule),
+        data: { preload: true }
+    },
+    {
+        path: 'management',
+        loadChildren: () => import('./management/management.module').then(m => m.ManagementModule),
+        data: { preload: false }
+    },
+    {
+        path: '', pathMatch: 'full', redirectTo: '/store/catalog'
+    }
+], { relativeLinkResolution: 'legacy' })
   ],
   exports: [RouterModule]
 })
