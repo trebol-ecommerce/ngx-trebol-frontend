4600ad7d5d986ce094761c59ec74b22608b32b72
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-09-21 00:57:03 -0300

    add BillingCompany entity

---
+++ src/app/models/entities/BillingCompany.ts
@@ -0,0 +1,9 @@
+// Copyright (c) 2020 Benjamin La Madrid
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+export class BillingCompany {
+  idNumber: string;
+  name: string;
+}
