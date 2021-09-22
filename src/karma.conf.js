8d0d95e117da35af6c99f207d77f94c3e4970da9
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-09-21 23:13:52 -0300

    update karma plugins and configuration file

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,32 +1,40 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+// https://karma-runner.github.io/6.3/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
-    basePath: '',
+    autoWatch: false,
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
+    logLevel: config.LOG_INFO,
+    port: 9876,
+    reporters: ['progress', 'kjhtml'],
+    restartOnFileChange: true, // when auto-watch is on, cancel and restart unit tests whenever a change is detected
+    singleRun: false,
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-firefox-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    coverageReporter: {
       dir: require('path').join(__dirname, '../coverage/trebol-ng'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ],
+      check: {
+        global: {
+          statements: 70,
+          branches: 70,
+          functions: 70,
+          lines: 70
+        }
+      }
+    }
   });
 };
