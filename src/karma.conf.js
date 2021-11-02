47d289d938f56ee92e18b266eaf90d0bf47d6c87
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-11-02 02:33:38 -0300

    include karma-edge-launcher in plugins

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,6 +16,7 @@ module.exports = function (config) {
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
+      require('karma-edge-launcher'),
       require('karma-firefox-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
