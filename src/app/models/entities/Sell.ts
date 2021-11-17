a4889bbafa7252cce412502b998d596c073857f2
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-11-17 11:44:12 -0300

    update Sell
    reorder properties alphabetically
    add properties included in latest API:
    - shipper
    - taxValue
    - token
    - totalItems
    - totalValue
    - transportValue

--- src/app/models/entities/Sell.ts
+++ src/app/models/entities/Sell.ts
@@ -10,18 +10,25 @@ import { BillingCompany } from './BillingCompany';
 import { Customer } from './Customer';
 import { Salesperson } from './Salesperson';
 import { SellDetail } from './SellDetail';
+import { Shipper } from './Shipper';
 
 export class Sell {
-  buyOrder?: number;
-  details: SellDetail[];
-  date?: Date;
-  billingType: string;
-  billingCompany?: BillingCompany;
-  paymentType: string;
-  customer: Customer;
-  netValue?: number;
-  salesperson?: Salesperson;
-  status?: string;
   billingAddress?: Address;
+  billingCompany?: BillingCompany;
+  billingType: string;
+  buyOrder?: number;
+  customer: Customer;
+  date?: Date;
+  details: SellDetail[];
+  netValue?: number;
+  paymentType: string;
+  salesperson?: Salesperson;
+  shipper?: Shipper;
   shippingAddress?: Address;
+  status?: string;
+  taxValue?: number;
+  token?: number;
+  totalItems?: number;
+  totalValue?: number;
+  transportValue?: number;
 }
