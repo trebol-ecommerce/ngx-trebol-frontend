d8203840432aad2bdb28e9170eac8c4facf098d2
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-12-16 00:17:59 -0300

    fix unit tests

--- src/app/shared/components/image-form/image-form.component.spec.ts
+++ src/app/shared/components/image-form/image-form.component.spec.ts
@@ -14,9 +14,6 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { of } from 'rxjs';
-import { API_SERVICE_INJECTION_TOKENS } from 'src/app/api/api-service-injection-tokens';
-import { IProductCategoriesDataApiService } from 'src/app/api/product-categories.data-api.iservice';
 import { ImageFormComponent } from './image-form.component';
 
 @Component({ selector: 'app-slideshow' })
@@ -30,17 +27,11 @@ class MockSlideshowComponent {
 describe('ImageFormComponent', () => {
   let component: ImageFormComponent;
   let fixture: ComponentFixture<ImageFormComponent>;
-  let mockCategoriesDataApiService: Partial<IProductCategoriesDataApiService>;
   let mockDialogRef: Partial<MatDialogRef<ImageFormComponent>>;
   let mockSnackBarService: Partial<MatSnackBar>;
   let mockDialogService: Partial<MatDialog>;
 
   beforeEach(waitForAsync(() => {
-    mockCategoriesDataApiService = {
-      readAllProductCategories() {
-        return of([]);
-      }
-    };
     mockDialogRef = {
       close() { }
     };
@@ -65,7 +56,6 @@ describe('ImageFormComponent', () => {
         MockSlideshowComponent
       ],
       providers: [
-        { provide: API_SERVICE_INJECTION_TOKENS.dataProductCategories, useValue: mockCategoriesDataApiService },
         { provide: MAT_DIALOG_DATA, useValue: null },
         { provide: MatDialogRef, useValue: mockDialogRef },
         { provide: MatSnackBar, useValue: mockSnackBarService },
--- src/app/shared/dialogs/input-dialog/input-dialog.component.spec.ts
+++ src/app/shared/dialogs/input-dialog/input-dialog.component.spec.ts
@@ -6,15 +6,28 @@
  */
 
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormControl } from '@angular/forms';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { InputDialogComponent } from './input-dialog.component';
+import { InputDialogData } from './InputDialogData';
 
 describe('InputDialogComponent', () => {
   let component: InputDialogComponent;
   let fixture: ComponentFixture<InputDialogComponent>;
+  let mockDialogData: InputDialogData;
 
   beforeEach(waitForAsync(() => {
+    mockDialogData = {
+      title: '',
+      hint: '',
+      formControl: new FormControl()
+    };
+
     TestBed.configureTestingModule({
-      declarations: [ InputDialogComponent ]
+      declarations: [InputDialogComponent],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: mockDialogData }
+      ]
     })
     .compileComponents();
   }));
