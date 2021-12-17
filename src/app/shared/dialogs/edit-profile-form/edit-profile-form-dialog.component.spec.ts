9774fd6aa723629cfe12601bab49e014f9e76a88
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-12-17 20:53:21 -0300

    tests: remove references to SharedModule
    follow same logic as commit 8012fa9

--- src/app/shared/dialogs/edit-profile-form/edit-profile-form-dialog.component.spec.ts
+++ src/app/shared/dialogs/edit-profile-form/edit-profile-form-dialog.component.spec.ts
@@ -5,44 +5,78 @@
  * https://opensource.org/licenses/MIT
  */
 
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { MatDialogRef } from '@angular/material/dialog';
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { of } from 'rxjs';
 import { Person } from 'src/models/entities/Person';
-import { SharedModule } from 'src/app/shared/shared.module';
 import { EditProfileFormDialogComponent } from './edit-profile-form-dialog.component';
 import { EditProfileFormService } from './edit-profile-form.service';
+
+@Component({ selector: 'app-centered-mat-spinner' })
+class MockCenteredMatSpinnerComponent { }
+
+@Component({
+  selector: 'app-person-form',
+  providers: [{ provide: NG_VALUE_ACCESSOR, multi: true, useExisting: MockPersonFormComponent }]
+})
+class MockPersonFormComponent
+  implements ControlValueAccessor {
+  onchange = (v: any) => { }
+  ontouched = () => { }
+  writeValue(obj: any): void { }
+  registerOnChange(fn: (v: any) => any): void { this.onchange = fn; }
+  registerOnTouched(fn: () => any): void { this.ontouched = fn; }
+}
 
 describe('EditProfileFormDialogComponent', () => {
   let component: EditProfileFormDialogComponent;
   let fixture: ComponentFixture<EditProfileFormDialogComponent>;
-  let service: Partial<EditProfileFormService>;
+  let mockService: Partial<EditProfileFormService>;
+  let mockDialog: Partial<MatDialogRef<EditProfileFormDialogComponent>>;
+  let mockSnackbarService: Partial<MatSnackBar>;
 
   beforeEach(waitForAsync(() => {
-    service = {
+    mockService = {
       saving$: of(false),
       confirmCancel$: of(false),
       loadProfile() { return of(new Person()); },
       saveProfile() { return of(true); },
       confirmCancel() { }
     };
+    mockSnackbarService = {
+      open() { return void 0; }
+    };
 
     TestBed.configureTestingModule({
       imports: [
-        SharedModule
+        NoopAnimationsModule,
+        CommonModule,
+        MatButtonModule,
+        MatDialogModule,
+        FormsModule,
+        ReactiveFormsModule
       ],
       declarations: [
-        EditProfileFormDialogComponent
+        EditProfileFormDialogComponent,
+        MockCenteredMatSpinnerComponent,
+        MockPersonFormComponent
       ],
       providers: [
-        { provide: MatDialogRef, useValue: {} }
+        { provide: EditProfileFormService, useValue: mockService },
+        { provide: MatDialogRef, useValue: mockDialog },
+        { provide: MatSnackBar, useValue: mockSnackbarService }
       ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    TestBed.overrideProvider(EditProfileFormService, { useValue: service });
     fixture = TestBed.createComponent(EditProfileFormDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
