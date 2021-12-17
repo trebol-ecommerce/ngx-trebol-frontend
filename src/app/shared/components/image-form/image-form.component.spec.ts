10ee6efdd3f147c3d13fce80be9f288f123b4753
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-12-17 20:58:16 -0300

    tests: fix image form unit test overencumberment

--- src/app/shared/components/image-form/image-form.component.spec.ts
+++ src/app/shared/components/image-form/image-form.component.spec.ts
@@ -5,62 +5,27 @@
  * https://opensource.org/licenses/MIT
  */
 
-import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { ImageFormComponent } from './image-form.component';
-
-@Component({ selector: 'app-slideshow' })
-class MockSlideshowComponent {
-  @Input() images: any[];
-  @Input() autocycle: boolean;
-  @Input() editable: boolean;
-  @Output() add = new EventEmitter();
-}
 
 describe('ImageFormComponent', () => {
   let component: ImageFormComponent;
   let fixture: ComponentFixture<ImageFormComponent>;
-  let mockDialogRef: Partial<MatDialogRef<ImageFormComponent>>;
-  let mockSnackBarService: Partial<MatSnackBar>;
-  let mockDialogService: Partial<MatDialog>;
 
   beforeEach(waitForAsync(() => {
-    mockDialogRef = {
-      close() { }
-    };
-    mockSnackBarService = {
-      open(m: string, a: string) { return void 0; }
-    };
-    mockDialogService = {
-      open() { return void 0; }
-    };
-
     TestBed.configureTestingModule({
       imports: [
         NoopAnimationsModule,
         FormsModule,
         ReactiveFormsModule,
         MatFormFieldModule,
-        MatInputModule,
-        MatSelectModule
+        MatInputModule
       ],
-      declarations: [
-        ImageFormComponent,
-        MockSlideshowComponent
-      ],
-      providers: [
-        { provide: MAT_DIALOG_DATA, useValue: null },
-        { provide: MatDialogRef, useValue: mockDialogRef },
-        { provide: MatSnackBar, useValue: mockSnackBarService },
-        { provide: MatDialog, useValue: mockDialogService }
-      ]
+      declarations: [ ImageFormComponent ]
     })
     .compileComponents();
   }));
