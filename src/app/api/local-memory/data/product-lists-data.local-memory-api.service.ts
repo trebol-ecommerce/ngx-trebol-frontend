05a47fdd6b5a69c7115eecd131413424e82b8823
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-12-28 00:06:17 -0300

    feat: implement product lists in fake api

---
+++ src/app/api/local-memory/data/product-lists-data.local-memory-api.service.ts
@@ -0,0 +1,124 @@
+/*
+ * Copyright (c) 2021 The Trébol eCommerce Project
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+import { Inject, Injectable } from '@angular/core';
+import { from, Observable, of, throwError } from 'rxjs';
+import { map, mapTo, switchMap, tap, toArray } from 'rxjs/operators';
+import { paginateItems } from 'src/functions/paginateItems';
+import { DataPage } from 'src/models/DataPage';
+import { Product } from 'src/models/entities/Product';
+import { ProductList } from 'src/models/entities/ProductList';
+import { API_SERVICE_INJECTION_TOKENS } from '../../api-service-injection-tokens';
+import { ITransactionalEntityDataApiService } from '../../transactional-entity.data-api.iservice';
+import { IProductListContentsDataApiService } from '../../transactional-product-lists.data.api.iservice';
+import {
+  matchesDateProperty, matchesIdProperty, matchesNumberProperty, matchesStringProperty
+} from '../entity-data.local-memory-api.functions';
+import { MOCK_PRODUCT_LISTS, MOCK_PRODUCT_LIST_CONTENTS_MAP } from '../mock/mock-product-lists.datasource';
+import { TransactionalEntityDataLocalMemoryApiService } from '../transactional-entity-data.local-memory-api.abstract.service';
+
+@Injectable()
+export class ProductListsDataLocalMemoryApiService
+  extends TransactionalEntityDataLocalMemoryApiService<ProductList>
+  implements IProductListContentsDataApiService {
+
+  protected items = MOCK_PRODUCT_LISTS.slice();
+  protected itemContentsMap = new Map([...MOCK_PRODUCT_LIST_CONTENTS_MAP.entries()]);
+
+  constructor(
+    @Inject(API_SERVICE_INJECTION_TOKENS.dataProducts) private productsApiService: ITransactionalEntityDataApiService<Product>
+  ) {
+    super();
+  }
+
+  fetchContents(list: ProductList, pageIndex?: number, pageSize?: number, sortBy?: string, order?: string): Observable<DataPage<Product>> {
+    if (!this.itemContentsMap.has(list.code)) {
+      return throwError({ status: 404 });
+    } else {
+      const fullListContents = this.itemContentsMap.get(list.code);
+      const totalCount = fullListContents.length;
+      const items = paginateItems(fullListContents, pageIndex, pageSize);
+      return of<DataPage<Product>>({
+        items,
+        totalCount,
+        pageIndex,
+        pageSize
+      });
+    }
+  }
+
+  addToContents(list: ProductList, productLike: Partial<Product>): Observable<any> {
+    if (!this.itemContentsMap.has(list.code)) {
+      return throwError({ status: 404 });
+    } else {
+      const fullListContents = this.itemContentsMap.get(list.code);
+      return this.productsApiService.fetchExisting(productLike).pipe(
+        tap(p => fullListContents.push(p)),
+        mapTo(void 0)
+      );
+    }
+  }
+
+  deleteFromContents(list: ProductList, productLike?: Partial<Product>): Observable<any> {
+    if (!this.itemContentsMap.has(list.code)) {
+      return throwError({ status: 404 });
+    } else {
+      const fullListContents = this.itemContentsMap.get(list.code);
+      const listItemIndex = fullListContents.findIndex(p => (p.barcode === productLike.barcode));
+      fullListContents.splice(listItemIndex);
+      return of(void 0);
+    }
+  }
+
+  updateContents(list: ProductList, products: Partial<Product>[]): Observable<any> {
+    if (!this.itemContentsMap.has(list.code)) {
+      return throwError({ status: 404 });
+    } else {
+      return from(products).pipe(
+        switchMap(p => this.productsApiService.fetchExisting(p)),
+        toArray(),
+        tap(products => this.itemContentsMap.set(list.code, products)),
+        mapTo(void 0)
+      );
+    }
+  }
+
+  protected itemExists(category: Partial<ProductList>): boolean {
+    return this.items.some(category2 => (category.code === category2.code));
+  }
+
+  protected getIndexOfItem(category: Partial<ProductList>): number {
+    return this.items.findIndex(category2 => (category.code === category2.code));
+  }
+
+  protected filterItems(filter: any): ProductList[] {
+    let matchingItems = this.items;
+    for (const propName in filter) {
+      if (filter.hasOwnProperty(propName)) {
+        const propValue = filter[propName];
+        if (propName === 'nameLike') {
+          const nameRegexp = new RegExp(`^.+${propValue}.+$`);
+          matchingItems = matchingItems.filter(c => nameRegexp.test(c.name));
+        } else if (propName !== 'id') {
+          if (typeof propValue === 'string') {
+            matchingItems = matchingItems.filter(it => matchesStringProperty(it, propName, propValue));
+          } else if (typeof propValue === 'number') {
+            matchingItems = matchingItems.filter(it => matchesNumberProperty(it, propName, propValue));
+          } else if (typeof propValue === 'object') {
+            if (propValue instanceof Date) {
+              matchingItems = matchingItems.filter(it => matchesDateProperty(it, propName, propValue));
+            } else if ('id' in propValue) {
+              matchingItems = matchingItems.filter(it => matchesIdProperty(it, propName, propValue));
+            }
+          }
+        }
+      }
+    }
+
+    return matchingItems;
+  }
+}
---
+++ src/app/api/local-memory/mock/mock-product-lists.datasource.ts
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2021 The Trébol eCommerce Project
+ *
+ * This software is released under the MIT License.
+ * https://opensource.org/licenses/MIT
+ */
+
+import { Product } from 'src/models/entities/Product';
+import { ProductList } from 'src/models/entities/ProductList';
+import { MOCK_PRODUCTS } from './mock-products.datasource';
+
+export const MOCK_PRODUCT_LISTS: ProductList[] = [
+  { code: 'OFERTAS', name: 'Ofertas ejemplo' }
+];
+
+export const MOCK_PRODUCT_LIST_CONTENTS_MAP = new Map<string, Product[]>([
+  [
+    'OFERTAS',
+    [
+      MOCK_PRODUCTS[0],
+      MOCK_PRODUCTS[1],
+      MOCK_PRODUCTS[2]
+    ]
+  ]
+]);
