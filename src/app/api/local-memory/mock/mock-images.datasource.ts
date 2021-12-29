56ee09344ba27362b7d110a8de5679d58c60dff7
Benjamin La Madrid <bg.lamadrid@gmail.com>
2021-12-29 06:32:25 -0300

    fix: nonexisting image refs in mock data

--- src/app/api/local-memory/mock/mock-images.datasource.ts
+++ src/app/api/local-memory/mock/mock-images.datasource.ts
@@ -7,20 +7,4 @@
 
 import { Image } from 'src/models/entities/Image';
 
-export const MOCK_IMAGES: Image[] = [
-  {
-    filename: 'photo-1578116922645-3976907a7671.jpg',
-    url: '/assets/img/products/photo-1578116922645-3976907a7671.jpg',
-    code: 'rAg313jtM8JQevlARoawDuo1mp/yok79q0Sxcw4uID01PLjyXLz5.'
-  },
-  {
-    filename: 'photo-1578172433613-9f1b258f7d5b.jpg',
-    url: '/assets/img/products/photo-1578172433613-9f1b258f7d5b.jpg',
-    code: 'Q7vh..wVJnP7MCTt86QAQ.MUbDLz.glztq5BHKj/O7ngG8Ew9gbQe'
-  },
-  {
-    filename: 'photo-1580143881495-b21dde95fc60.jpg',
-    url: '/assets/img/products/photo-1580143881495-b21dde95fc60.jpg',
-    code: 'R8nYg2a.4141xWBa7DtiUOxwahQIelyN.9eYOx4hbzUozySvjs4mq'
-  }
-];
+export const MOCK_IMAGES: Image[] = [ ];
