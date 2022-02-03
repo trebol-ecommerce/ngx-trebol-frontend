a4d79421ecfd3ae04ad323d62f1f4ff3223a4a2b
Benjamin La Madrid <bg.lamadrid@gmail.com>
2022-02-03 05:55:04 -0300

    fix(mock-data): receipts bad refs

--- src/app/api/local-memory/mock/mock-receipts.datasource.ts
+++ src/app/api/local-memory/mock/mock-receipts.datasource.ts
@@ -9,26 +9,21 @@ import { Receipt } from "src/models/Receipt";
 import { MOCK_PRODUCTS } from "./mock-products.datasource";
 import { MOCK_SELL_STATUSES } from "./mock-sell-statuses.datasource";
 
-const MOCK_PRODUCT_UNIT_VALUES = [
-  12990,
-  14990
-];
-
 export const MOCK_RECEIPTS: Receipt[] = [
   {
     buyOrder: 1,
     date: '2021-06-16',
     details: [
-      { product: MOCK_PRODUCTS[0], units: 4, unitValue: MOCK_PRODUCT_UNIT_VALUES[0] },
-      { product: MOCK_PRODUCTS[1], units: 1, unitValue: MOCK_PRODUCT_UNIT_VALUES[1] },
+      { product: MOCK_PRODUCTS[0], units: 4, unitValue: MOCK_PRODUCTS[0].price },
+      { product: MOCK_PRODUCTS[1], units: 1, unitValue: MOCK_PRODUCTS[1].price },
     ],
     taxValue: 12000,
     transportValue: 5000,
     status: MOCK_SELL_STATUSES.delivered,
     totalItems: 5,
     totalValue: ( 5000 + 12000 +
-      (MOCK_PRODUCT_UNIT_VALUES[0] * 4) +
-      MOCK_PRODUCT_UNIT_VALUES[1]
+      (MOCK_PRODUCTS[0].price * 4) +
+      MOCK_PRODUCTS[1].price
     ),
     token: 'I6EqPVxQjqt0ZExl4vvnEYEq3q1Mnqi'
   },
@@ -36,14 +31,14 @@ export const MOCK_RECEIPTS: Receipt[] = [
     buyOrder: 2,
     date: '2021-06-18',
     details: [
-      { product: MOCK_PRODUCTS[0], units: 2, unitValue: MOCK_PRODUCT_UNIT_VALUES[0] }
+      { product: MOCK_PRODUCTS[0], units: 2, unitValue: MOCK_PRODUCTS[0].price }
     ],
     taxValue: 8000,
     transportValue: 3000,
     status: MOCK_SELL_STATUSES.delivered,
     totalItems: 2,
     totalValue: ( 3000 + 8000 +
-      (MOCK_PRODUCT_UNIT_VALUES[0] * 2)
+      (MOCK_PRODUCTS[0].price * 2)
     ),
     token: 'KuwfOAe5sayZHMOar5.5yb6RyahwdUW'
   },
@@ -51,14 +46,14 @@ export const MOCK_RECEIPTS: Receipt[] = [
     buyOrder: 3,
     date: '2021-06-18',
     details: [
-      { product: MOCK_PRODUCTS[1], units: 1, unitValue: MOCK_PRODUCT_UNIT_VALUES[1] },
+      { product: MOCK_PRODUCTS[1], units: 1, unitValue: MOCK_PRODUCTS[1].price },
     ],
     taxValue: 6000,
     transportValue: 3000,
     status: MOCK_SELL_STATUSES.deliveryCancelled,
     totalItems: 1,
     totalValue: ( 3000 + 6000 +
-      MOCK_PRODUCT_UNIT_VALUES[1]
+      MOCK_PRODUCTS[1].price
     ),
     token: 'uDq8.SvZ4.cTbKLAkVFHJZy0cYiyKjW'
   },
@@ -66,14 +61,14 @@ export const MOCK_RECEIPTS: Receipt[] = [
     buyOrder: 4,
     date: '2021-06-22',
     details: [
-      { product: MOCK_PRODUCTS[0], units: 2, unitValue: MOCK_PRODUCT_UNIT_VALUES[0] },
+      { product: MOCK_PRODUCTS[0], units: 2, unitValue: MOCK_PRODUCTS[0].price },
     ],
     taxValue: 6000,
     transportValue: 3000,
     status: MOCK_SELL_STATUSES.unpaid,
     totalItems: 2,
     totalValue: ( 3000 + 6000 +
-      (MOCK_PRODUCT_UNIT_VALUES[0] * 2)
+      (MOCK_PRODUCTS[0].price * 2)
     ),
     token: 'lNeY5IlPL4dHC4XzdzjmK5I.4IvjnZC'
   },
@@ -81,16 +76,16 @@ export const MOCK_RECEIPTS: Receipt[] = [
     buyOrder: 5,
     date: '2021-07-03',
     details: [
-      { product: MOCK_PRODUCTS[0], units: 1, unitValue: MOCK_PRODUCT_UNIT_VALUES[0] },
-      { product: MOCK_PRODUCTS[2], units: 1, unitValue: MOCK_PRODUCT_UNIT_VALUES[2] },
+      { product: MOCK_PRODUCTS[0], units: 1, unitValue: MOCK_PRODUCTS[0].price },
+      { product: MOCK_PRODUCTS[2], units: 1, unitValue: MOCK_PRODUCTS[2].price },
     ],
     taxValue: 6000,
     transportValue: 3000,
     status: MOCK_SELL_STATUSES.failed,
     totalItems: 2,
     totalValue: ( 3000 + 6000 +
-      MOCK_PRODUCT_UNIT_VALUES[0] +
-      MOCK_PRODUCT_UNIT_VALUES[2]
+      MOCK_PRODUCTS[0].price +
+      MOCK_PRODUCTS[2].price
     ),
     token: '3Db3oTVDoriYx8J//Ul52aDcGX1JxF.'
   }
