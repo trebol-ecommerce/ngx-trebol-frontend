1fffe42d7432eafacbe3415f1a5b75367f43909a
Benjamin La Madrid <bg.lamadrid@gmail.com>
2022-02-06 23:17:19 -0300

    fix(mock-data): translate & mark literal text
    update localization files

--- src/app/api/local-memory/mock/mock-company-details.examples.ts
+++ src/app/api/local-memory/mock/mock-company-details.examples.ts
@@ -8,8 +8,8 @@
 import { CompanyDetails } from 'src/models/CompanyDetails';
 
 export const MOCK_COMPANY_DETAILS: CompanyDetails = {
-  name: 'Trébol Demo',
-  description: 'Esta información está almacenada en archivos locales y sirve para dar una demostración verosímil y aceptable de las características de la tienda virtual Trébol.',
+  name: $localize`:Example of a company or business name:Some company or business name`,
+  description: $localize`:Example of a paragraph used to describe the business this application is meant for:This paragraph should describe your business profile, motivation, goals, success stories, whatever helps build an image.`,
   bannerImageURL: 'https://fakeimg.pl/600x200',
   logoImageURL: 'https://fakeimg.pl/400x400'
 };
