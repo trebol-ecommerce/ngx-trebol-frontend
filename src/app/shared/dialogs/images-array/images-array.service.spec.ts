d35aa67e568f8831e32e9b9c1c3ad6f22f238936
Benjamin La Madrid <bg.lamadrid@gmail.com>
2022-03-10 15:58:52 -0300

    test: fix ImagesArrayService observable unit test

--- src/app/shared/dialogs/images-array/images-array.service.spec.ts
+++ src/app/shared/dialogs/images-array/images-array.service.spec.ts
@@ -7,7 +7,7 @@
 
 import { TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
-import { take } from 'rxjs/operators';
+import { take, tap } from 'rxjs/operators';
 import { API_SERVICE_INJECTION_TOKENS } from 'src/app/api/api-service-injection-tokens';
 import { IEntityDataApiService } from 'src/app/api/entity.data-api.iservice';
 import { DataPage } from 'src/models/DataPage';
@@ -45,7 +45,7 @@ describe('ImagesArrayService', () => {
   });
 
   it('should expose an observable of images', () => {
-    const mockImagesArray: DataPage<Image> = {
+    const mockDataPage: DataPage<Image> = {
       items: [
         {
           filename: 'test.png',
@@ -56,14 +56,17 @@ describe('ImagesArrayService', () => {
       pageSize: 10,
       totalCount: 1
     };
-    const mockService = { images$: of(mockImagesArray) };
-    TestBed.overrideProvider(API_SERVICE_INJECTION_TOKENS.dataImages, { useValue: mockService });
+    mockApiService.fetchPage = () => of(mockDataPage);
+    TestBed.overrideProvider(
+      API_SERVICE_INJECTION_TOKENS.dataImages,
+      { useValue: mockApiService }
+    );
     service = TestBed.inject(ImagesArrayService);
 
-    service.imagesPage$.pipe(take(1)).subscribe(
-      next => {
-        expect(next).toEqual(mockImagesArray);
-      }
-    );
+    service.reloadItems();
+    service.imagesPage$.pipe(
+      take(1),
+      tap(nextPage => expect(nextPage).toEqual(mockDataPage))
+    ).subscribe();
   });
 });
