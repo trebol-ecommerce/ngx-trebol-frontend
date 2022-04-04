f6983b4873b258c0147bd1caead1f5eb4c57b8ff
Benjamin La Madrid <bg.lamadrid@gmail.com>
2022-04-04 16:55:53 -0400

    chore: clean up commented code

--- src/app/store/components/header/brand/store-header-brand.component.ts
+++ src/app/store/components/header/brand/store-header-brand.component.ts
@@ -6,9 +6,6 @@
  */
 
 import { Component } from '@angular/core';
-// import { MatDialog } from '@angular/material/dialog';
-// import { Router } from '@angular/router';
-// import { StoreCompanyDetailsDialogComponent } from 'src/app/store/dialogs/company-details/store-company-details-dialog.component';
 import { environment } from 'src/environments/environment';
 import { Image } from 'src/models/entities/Image';
 
@@ -22,14 +19,6 @@ export class StoreHeaderBrandComponent {
   readonly appTitle: string = environment.labels.name;
   readonly appLogo: Image = environment.staticImages.logo;
 
-  constructor(
-    // private router: Router
-  ) { }
-
-  onClickViewCompanyDetails(): void {
-    // this.dialogService.open(
-    //   StoreCompanyDetailsDialogComponent
-    // );
-  }
+  constructor() { }
 
 }
