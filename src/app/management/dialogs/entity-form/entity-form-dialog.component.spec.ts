356b27fe0b9b6389243b31a883c41aa07bf4b917
Benjamin La Madrid <bg.lamadrid@gmail.com>
2022-04-06 12:54:44 -0400

    test: improve entity form dialog suite

--- src/app/management/dialogs/entity-form/entity-form-dialog.component.spec.ts
+++ src/app/management/dialogs/entity-form/entity-form-dialog.component.spec.ts
@@ -5,9 +5,9 @@
  * https://opensource.org/licenses/MIT
  */
 
-import { Component, forwardRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import { NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -15,6 +15,7 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { of } from 'rxjs';
 import { ITransactionalEntityDataApiService } from 'src/app/api/transactional-entity.data-api.iservice';
 import { EntityFormGroupFactoryService } from 'src/app/shared/entity-form-group-factory.service';
+import { EntityTypeName } from 'src/models/EntityTypeNames';
 import { EntityFormDialogComponent } from './entity-form-dialog.component';
 import { EntityFormDialogData } from './EntityFormDialogData';
 
@@ -23,136 +24,72 @@ class MockCenteredMatSpinnerComponent { }
 
 @Component({
   selector: 'app-address-form',
-  providers: [{ provide: NG_VALUE_ACCESSOR, multi: true, useExisting: forwardRef(() => MockAddressFormComponent) }]
+  providers: [{ provide: NG_VALUE_ACCESSOR, multi: true, useExisting: MockAddressFormComponent }]
 })
-class MockAddressFormComponent
-  implements ControlValueAccessor {
-  writeValue(v: any) { }
-  registerOnChange() { }
-  registerOnTouched() { }
-  setDisabledState() { }
-}
+class MockAddressFormComponent { }
 
 @Component({
   selector: 'app-image-form',
   providers: [{ provide: NG_VALUE_ACCESSOR, multi: true, useExisting: MockImageFormComponent }]
 })
-class MockImageFormComponent
-  implements ControlValueAccessor {
-  writeValue(v: any) { }
-  registerOnChange() { }
-  registerOnTouched() { }
-  setDisabledState() { }
-}
+class MockImageFormComponent { }
 
 @Component({
   selector: 'app-person-form',
   providers: [{ provide: NG_VALUE_ACCESSOR, multi: true, useExisting: MockPersonFormComponent }]
 })
-class MockPersonFormComponent
-  implements ControlValueAccessor {
-  writeValue(v: any) { }
-  registerOnChange() { }
-  registerOnTouched() { }
-  setDisabledState() { }
-}
+class MockPersonFormComponent { }
 
 @Component({
   selector: 'app-product-form',
   providers: [{ provide: NG_VALUE_ACCESSOR, multi: true, useExisting: MockProductFormComponent }]
 })
-class MockProductFormComponent
-  implements ControlValueAccessor {
-  writeValue(v: any) { }
-  registerOnChange() { }
-  registerOnTouched() { }
-  setDisabledState() { }
-}
+class MockProductFormComponent { }
 
 @Component({
   selector: 'app-product-category-form',
   providers: [{ provide: NG_VALUE_ACCESSOR, multi: true, useExisting: MockProductCategoryFormComponent }]
 })
-class MockProductCategoryFormComponent
-  implements ControlValueAccessor {
-  writeValue(v: any) { }
-  registerOnChange() { }
-  registerOnTouched() { }
-  setDisabledState() { }
-}
+class MockProductCategoryFormComponent { }
 
 @Component({
   selector: 'app-product-list-form',
   providers: [{ provide: NG_VALUE_ACCESSOR, multi: true, useExisting: MockProductListFormComponent }]
 })
-class MockProductListFormComponent
-  implements ControlValueAccessor {
-  writeValue(v: any) { }
-  registerOnChange() { }
-  registerOnTouched() { }
-  setDisabledState() { }
-}
+class MockProductListFormComponent { }
 
 @Component({
   selector: 'app-sell-form',
   providers: [{ provide: NG_VALUE_ACCESSOR, multi: true, useExisting: MockSellFormComponent }]
 })
-class MockSellFormComponent
-  implements ControlValueAccessor {
-  writeValue(v: any) { }
-  registerOnChange() { }
-  registerOnTouched() { }
-  setDisabledState() { }
-}
+class MockSellFormComponent { }
 
 @Component({
   selector: 'app-shipper-form',
   providers: [{ provide: NG_VALUE_ACCESSOR, multi: true, useExisting: MockShipperFormComponent }]
 })
-class MockShipperFormComponent
-  implements ControlValueAccessor {
-  writeValue(v: any) { }
-  registerOnChange() { }
-  registerOnTouched() { }
-  setDisabledState() { }
-}
+class MockShipperFormComponent { }
 
 @Component({
   selector: 'app-user-form',
   providers: [{ provide: NG_VALUE_ACCESSOR, multi: true, useExisting: MockUserFormComponent }]
 })
-class MockUserFormComponent
-  implements ControlValueAccessor {
-  writeValue(v: any) { }
-  registerOnChange() { }
-  registerOnTouched() { }
-  setDisabledState() { }
-}
+class MockUserFormComponent { }
 
 describe('EntityFormDialogComponent', () => {
   let component: EntityFormDialogComponent<any>;
   let fixture: ComponentFixture<EntityFormDialogComponent<any>>;
-  let mockDataApiService: ITransactionalEntityDataApiService<any>;
+  let dataApiServiceSpy: jasmine.SpyObj<ITransactionalEntityDataApiService<any>>;
+  let dialogRefSpy: jasmine.SpyObj<MatDialogRef<EntityFormDialogComponent<any>>>;
+  let snackBarServiceSpy: jasmine.SpyObj<MatSnackBar>;
   let mockDialogData: Partial<EntityFormDialogData<any>>;
-  let mockDialogRef: Partial<MatDialogRef<EntityFormDialogComponent<any>>>;
-  let mockSnackBarService: Partial<MatSnackBar>;
 
   beforeEach(waitForAsync(() => {
-    mockDataApiService = {
-      create() { return of(void 0); },
-      delete() { return of(void 0); },
-      fetchExisting() { return of(void 0); },
-      fetchPage() { return of(void 0); },
-      update() { return of(void 0); }
-    };
+    const mockDataApiService = jasmine.createSpyObj('ITransactionalEntityDataApiService<any>', ['create', 'update']);
+    const mockDialogRef = jasmine.createSpyObj('MatDialogRef<EntityFormDialogComponent<any>>', ['close']);
+    const mockSnackBarService = jasmine.createSpyObj('MatSnackBar', ['open']);
     mockDialogData = {
       apiService: mockDataApiService
-    };
-    mockDialogRef = {
-      close() { }
-    };
-    mockSnackBarService = {
-      open(m: string, a: string) { return void 0; }
     };
 
     TestBed.configureTestingModule({
@@ -182,87 +119,90 @@ describe('EntityFormDialogComponent', () => {
         EntityFormGroupFactoryService
       ]
     }).compileComponents();
-    fixture = TestBed.createComponent(EntityFormDialogComponent);
-    component = fixture.componentInstance;
   }));
 
-  it('should error out without a correct entity type', () => {
-    try {
+  beforeEach(() => {
+    dataApiServiceSpy = TestBed.inject(MAT_DIALOG_DATA) as jasmine.SpyObj<ITransactionalEntityDataApiService<any>>;
+    dialogRefSpy = TestBed.inject(MatDialogRef) as jasmine.SpyObj<MatDialogRef<EntityFormDialogComponent<any>>>;
+    snackBarServiceSpy = TestBed.inject(MatSnackBar) as jasmine.SpyObj<MatSnackBar>;
+
+    fixture = TestBed.createComponent(EntityFormDialogComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should error out if no entity type is passed', () => {
+    expect(() => {
       fixture.detectChanges();
-    } catch (error) {
+    }).toThrow();
+  });
+
+  describe('always', () => {
+    beforeEach(() => {
+      try {
+        fixture.detectChanges();
+      } catch (err) { }
+    });
+
+    it('should create', () => {
       expect(component).toBeTruthy();
-    }
+    });
+
+    it('should close upon cancellation', () => {
+      component.onCancel();
+      expect(dialogRefSpy.close).toHaveBeenCalled();
+    })
   });
 
-  it('should create a correct Address form', () => {
-    mockDialogData.entityType = 'address';
-    fixture.detectChanges();
-    const nativeElem: HTMLElement = fixture.nativeElement;
-    const formElem = nativeElem.querySelector('app-address-form');
-    expect(formElem).toBeTruthy();
-  });
+  describe('when an entity type is passed', () => {
+    let shouldCreateForm: (entityType: EntityTypeName, selector?: string) => void;
 
-  it('should create a correct Image form', () => {
-    mockDialogData.entityType = 'image';
-    fixture.detectChanges();
-    const nativeElem: HTMLElement = fixture.nativeElement;
-    const formElem = nativeElem.querySelector('app-image-form');
-    expect(formElem).toBeTruthy();
-  });
+    beforeEach(() => {
+      shouldCreateForm = (entityType: EntityTypeName, selector?: string) => {
+        if (!selector) {
+          selector = `app-${entityType}-form`;
+        }
+        mockDialogData.entityType = entityType;
+        fixture.detectChanges();
+        expect(component.innerFormGroup).toBeTruthy();
+        const formElem = fixture.debugElement.nativeElement.querySelector(selector);
+        expect(formElem).toBeTruthy();
+      };
+    });
 
-  it('should create a correct Person form', () => {
-    mockDialogData.entityType = 'person';
-    fixture.detectChanges();
-    const nativeElem: HTMLElement = fixture.nativeElement;
-    const formElem = nativeElem.querySelector('app-person-form');
-    expect(formElem).toBeTruthy();
-  });
+    it('should be able to create an Address form', () => {
+      shouldCreateForm('address');
+    });
 
-  it('should create a correct Product form', () => {
-    mockDialogData.entityType = 'product';
-    fixture.detectChanges();
-    const nativeElem: HTMLElement = fixture.nativeElement;
-    const formElem = nativeElem.querySelector('app-product-form');
-    expect(formElem).toBeTruthy();
-  });
+    it('should be able to create an Image form', () => {
+      shouldCreateForm('image');
+    });
 
-  it('should create a correct ProductCategory form', () => {
-    mockDialogData.entityType = 'productCategory';
-    fixture.detectChanges();
-    const nativeElem: HTMLElement = fixture.nativeElement;
-    const formElem = nativeElem.querySelector('app-product-category-form');
-    expect(formElem).toBeTruthy();
-  });
+    it('should be able to create a Person form', () => {
+      shouldCreateForm('person');
+    });
 
-  it('should create a correct ProductList form', () => {
-    mockDialogData.entityType = 'productList';
-    fixture.detectChanges();
-    const nativeElem: HTMLElement = fixture.nativeElement;
-    const formElem = nativeElem.querySelector('app-product-list-form');
-    expect(formElem).toBeTruthy();
-  });
+    it('should be able to create a Product form', () => {
+      shouldCreateForm('product');
+    });
 
-  it('should create a correct Sell form', () => {
-    mockDialogData.entityType = 'sell';
-    fixture.detectChanges();
-    const nativeElem: HTMLElement = fixture.nativeElement;
-    const formElem = nativeElem.querySelector('app-sell-form');
-    expect(formElem).toBeTruthy();
-  });
+    it('should be able to create a ProductCategory form', () => {
+      shouldCreateForm('productCategory', 'app-product-category-form');
+    });
 
-  it('should create a correct Shipper form', () => {
-    mockDialogData.entityType = 'shipper';
-    fixture.detectChanges();
-    const nativeElem: HTMLElement = fixture.nativeElement;
-    const formElem = nativeElem.querySelector('app-shipper-form');
-    expect(formElem).toBeTruthy();
-  });
+    it('should be able to create a ProductList form', () => {
+      shouldCreateForm('productList', 'app-product-list-form');
+    });
 
-  it('should create a correct User form', () => {
-    mockDialogData.entityType = 'user';
-    fixture.detectChanges();
-    const nativeElem: HTMLElement = fixture.nativeElement;
-    const formElem = nativeElem.querySelector('app-user-form');
-    expect(formElem).toBeTruthy();
+    it('should be able to create a Sell form', () => {
+      shouldCreateForm('sell');
+    });
+
+    it('should be able to create a Shipper form', () => {
+      shouldCreateForm('shipper');
+    });
+
+    it('should be able to create a User form', () => {
+      shouldCreateForm('user');
+    });
   });
 });
