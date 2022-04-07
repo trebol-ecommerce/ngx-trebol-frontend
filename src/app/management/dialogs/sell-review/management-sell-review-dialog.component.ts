2471c75a271711a38bf47b47ba815948a7f0ae01
Benjamin La Madrid <bg.lamadrid@gmail.com>
2022-04-07 03:25:15 -0400

    chore: tidy up some rxjs code

--- src/app/management/dialogs/sell-review/management-sell-review-dialog.component.ts
+++ src/app/management/dialogs/sell-review/management-sell-review-dialog.component.ts
@@ -8,8 +8,8 @@
 import { Component, Inject, OnDestroy } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { BehaviorSubject, Observable, Subscription, throwError } from 'rxjs';
-import { catchError, finalize, switchMap, tap } from 'rxjs/operators';
+import { BehaviorSubject, Observable, Subscription } from 'rxjs';
+import { finalize, switchMap, tap } from 'rxjs/operators';
 import { COMMON_DISMISS_BUTTON_LABEL, COMMON_ERROR_MESSAGE } from 'src/text/messages';
 import { SELL_STATUS_NAMES_MAP } from 'src/text/sell-status-names';
 import { ManagementSalesService } from '../../routes/sales/management-sales.service';
@@ -46,42 +46,48 @@ export class ManagementSellReviewDialogComponent
 
   onClickReject(): void {
     this.operationSub?.unsubscribe();
+    this.busyStatusSource.next(true);
     this.operationSub = this.updateSellDataAfter(
       this.salesService.markRejected(this.data.sell),
       $localize`:Message of success after rejecting an order:Sell was rejected`
+    ).pipe(
+      finalize(() => this.busyStatusSource.next(false))
     ).subscribe();
   }
 
   onClickConfirm(): void {
     this.operationSub?.unsubscribe();
+    this.busyStatusSource.next(true);
     this.operationSub = this.updateSellDataAfter(
       this.salesService.markConfirmed(this.data.sell),
       $localize`:Message of success after confirming an order:Sell was confirmed`
+    ).pipe(
+      finalize(() => this.busyStatusSource.next(false))
     ).subscribe();
   }
 
   onClickComplete(): void {
     this.operationSub?.unsubscribe();
+    this.busyStatusSource.next(true);
     this.operationSub = this.updateSellDataAfter(
       this.salesService.markComplete(this.data.sell),
       $localize`:Message of success after completing an order:Congratulations! Sell is complete`
+    ).pipe(
+      finalize(() => this.busyStatusSource.next(false))
     ).subscribe();
   }
 
   private updateSellDataAfter(observable: Observable<any>, successMessage: string) {
     return observable.pipe(
-      tap(() => {
-        this.busyStatusSource.next(true);
-        this.salesService.reloadItems();
-        this.snackBarService.open(successMessage, COMMON_DISMISS_BUTTON_LABEL);
-      }),
       switchMap(() => this.salesService.fetch(this.data.sell)),
-      tap(next => { this.data.sell = next; }),
-      catchError(err => {
-        this.snackBarService.open(COMMON_ERROR_MESSAGE, COMMON_DISMISS_BUTTON_LABEL);
-        return throwError(err);
-      }),
-      finalize(() => this.busyStatusSource.next(false))
+      tap(
+        sell => {
+          this.data.sell = sell;
+          this.salesService.reloadItems();
+          this.snackBarService.open(successMessage, COMMON_DISMISS_BUTTON_LABEL);
+        },
+        err => this.snackBarService.open(COMMON_ERROR_MESSAGE, COMMON_DISMISS_BUTTON_LABEL)
+      )
     );
   }
 
