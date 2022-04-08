77d8970fd55eaf3ac8bfd513fe366f527adc2f7d
Benjamin La Madrid <bg.lamadrid@gmail.com>
2022-04-08 18:13:10 -0400

    test: improve coverage for company-form

--- src/app/shared/forms/company/company-form.component.spec.ts
+++ src/app/shared/forms/company/company-form.component.spec.ts
@@ -5,16 +5,30 @@
  * https://opensource.org/licenses/MIT
  */
 
+import { Component, ViewChild } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BillingCompany } from 'src/models/entities/BillingCompany';
 import { CompanyFormComponent } from './company-form.component';
 
+@Component({
+  selector: 'app-higher-order-form',
+  template: '<form [formGroup]="formGroup"><app-company-form formControlName="company"></app-company-form></form>'
+})
+class MockHigherOrderFormComponent {
+  @ViewChild(CompanyFormComponent, { static: true }) companyFormComponent: CompanyFormComponent;
+
+  formGroup = new FormGroup({ company: new FormControl(null) });
+  get company() { return this.formGroup.get('company') as FormControl; }
+}
+
 describe('CompanyFormComponent', () => {
+  let containerForm: MockHigherOrderFormComponent;
+  let fixture: ComponentFixture<MockHigherOrderFormComponent>;
   let component: CompanyFormComponent;
-  let fixture: ComponentFixture<CompanyFormComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -24,18 +38,53 @@ describe('CompanyFormComponent', () => {
         MatFormFieldModule,
         MatInputModule
       ],
-      declarations: [ CompanyFormComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        CompanyFormComponent,
+        MockHigherOrderFormComponent
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(CompanyFormComponent);
-    component = fixture.componentInstance;
+    fixture = TestBed.createComponent(MockHigherOrderFormComponent);
+    containerForm = fixture.componentInstance;
+    component = containerForm.companyFormComponent;
     fixture.detectChanges();
   });
 
   it('should create', () => {
+    expect(containerForm).toBeTruthy();
     expect(component).toBeTruthy();
+  });
+
+  it('should not be valid at creation time', () => {
+    expect(containerForm.formGroup.invalid).toBeTrue();
+    expect(component.formGroup.invalid).toBeTrue();
+  });
+
+  it('should accept instances of Person as valid input', () => {
+    const mockCompany: BillingCompany = {
+      name: 'some-name',
+      idNumber: 'some-id-number'
+    };
+    containerForm.company.setValue(mockCompany);
+    expect(component.formGroup.value).toEqual(mockCompany);
+    expect(component.formGroup.valid).toBeTrue();
+  });
+
+  it('should treat non-Person-instances as invalid input', () => {
+    const notACompany = {
+      foo: 'example',
+      bar: 'test'
+    };
+    containerForm.company.setValue(notACompany);
+    expect(component.formGroup.invalid).toBeTrue();
+  });
+
+  it('should respond to changes in disabled state', () => {
+    containerForm.formGroup.disable();
+    expect(component.formGroup.disabled).toBeTrue();
+    containerForm.formGroup.enable();
+    expect(component.formGroup.enabled).toBeTrue();
   });
 });
