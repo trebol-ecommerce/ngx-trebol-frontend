02685b0b7ed323861a52b4edab14cffd64fd86c5
Benjamin La Madrid <bg.lamadrid@gmail.com>
2022-04-10 15:30:28 -0400

    test: improve coverage for product-card

--- src/app/shared/components/product-card/product-card.component.spec.ts
+++ src/app/shared/components/product-card/product-card.component.spec.ts
@@ -9,6 +9,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { count, take, tap } from 'rxjs/operators';
 import { ProductCardComponent } from './product-card.component';
 
 describe('ProductCardComponent', () => {
@@ -23,8 +24,7 @@ describe('ProductCardComponent', () => {
         MatIconModule
       ],
       declarations: [ ProductCardComponent ],
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -35,5 +35,23 @@ describe('ProductCardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fire `addToCart` when clicking in its add-product button', () => {
+    component.addToCart.pipe(
+      take(1),
+      count(),
+      tap(c => expect(c).toBe(1))
+    ).subscribe();
+    component.onClickAddProduct();
+  });
+
+  it('should fire `view` when clicking in its view-product button', () => {
+    component.view.pipe(
+      take(1),
+      count(),
+      tap(c => expect(c).toBe(1))
+    ).subscribe();
+    component.onClickViewProduct();
   });
 });
