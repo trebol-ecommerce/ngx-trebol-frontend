a630bfdec0de8d9c4bd5e8458f4d4763d3b38cda
Benjamin La Madrid <bg.lamadrid@gmail.com>
2022-04-12 19:01:44 -0400

    fix: category picker dialog class bindings

--- src/app/shared/dialogs/product-category-picker/product-category-picker-dialog.component.ts
+++ src/app/shared/dialogs/product-category-picker/product-category-picker-dialog.component.ts
@@ -5,28 +5,29 @@
  * https://opensource.org/licenses/MIT
  */
 
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { API_INJECTION_TOKENS } from 'src/app/api/api-injection-tokens';
 import { ITransactionalEntityDataApiService } from 'src/app/api/transactional-entity.data-api.iservice';
 import { ProductCategory } from 'src/models/entities/ProductCategory';
+import { ProductCategoryTreeService } from '../../components/product-category-tree/product-category-tree.service';
 
 @Component({
   selector: 'app-product-category-picker-dialog',
   templateUrl: './product-category-picker-dialog.component.html',
   styleUrls: ['./product-category-picker-dialog.component.css']
 })
-export class ProductCategoryPickerDialogComponent {
-
-  categories$: Observable<ProductCategory[]>;
+export class ProductCategoryPickerDialogComponent
+  implements OnInit {
 
   constructor(
     @Inject(API_INJECTION_TOKENS.dataProductCategories) private apiService: ITransactionalEntityDataApiService<ProductCategory>,
-    private dialog: MatDialogRef<ProductCategoryPickerDialogComponent>
-  ) {
-    this.categories$ = this.apiService.fetchPage().pipe(map(next => next.items));
+    private dialog: MatDialogRef<ProductCategoryPickerDialogComponent>,
+    private categoryTreeService: ProductCategoryTreeService
+  ) { }
+
+  ngOnInit(): void {
+    this.categoryTreeService.reloadCategories();
   }
 
   onSelect(option: ProductCategory) {
