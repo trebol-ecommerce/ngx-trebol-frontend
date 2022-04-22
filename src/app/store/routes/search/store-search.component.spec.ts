90edb78a4cf7ef0e2e3629255b87b2583ac9cf52
Benjamin La Madrid <bg.lamadrid@gmail.com>
2022-04-22 19:50:18 -0400

    test: improve coverage for store search route comp

--- src/app/store/routes/search/store-search.component.spec.ts
+++ src/app/store/routes/search/store-search.component.spec.ts
@@ -8,9 +8,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { MatIconModule } from '@angular/material/icon';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { of } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { EMPTY, of } from 'rxjs';
+import { MOCK_PRODUCTS } from 'src/app/api/local-memory/mock/mock-products.datasource';
 import { Product } from 'src/models/entities/Product';
 import { StoreCartService } from '../../store-cart.service';
 import { StoreSearchService } from '../../store-search.service';
@@ -34,11 +36,15 @@ describe('StoreSearchComponent', () => {
   let searchServiceSpy: jasmine.SpyObj<StoreSearchService>;
   let cartServiceSpy: jasmine.SpyObj<StoreCartService>;
   let catalogServiceSpy: jasmine.SpyObj<StoreCatalogService>;
+  let mockActivatedRoute: Partial<ActivatedRoute>;
 
   beforeEach(waitForAsync(() => {
     const mockSearchService = jasmine.createSpyObj('StoreSearchService', ['updateSearchQuery', 'reload', 'paginate']);
     const mockCartService = jasmine.createSpyObj('StoreCartService', ['addProductToCart']);
     const mockStoreCatalogService = jasmine.createSpyObj('StoreCatalogService', ['navigateToProductDetails']);
+    mockActivatedRoute = {
+      queryParams: of({})
+    };
 
     TestBed.configureTestingModule({
       imports: [
@@ -54,7 +60,8 @@ describe('StoreSearchComponent', () => {
       providers: [
         { provide: StoreSearchService, useValue: mockSearchService },
         { provide: StoreCartService, useValue: mockCartService },
-        { provide: StoreCatalogService, useValue: mockStoreCatalogService }
+        { provide: StoreCatalogService, useValue: mockStoreCatalogService },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute }
       ]
     }).compileComponents();
   }));
@@ -63,6 +70,8 @@ describe('StoreSearchComponent', () => {
     searchServiceSpy = TestBed.inject(StoreSearchService) as jasmine.SpyObj<StoreSearchService>;
     cartServiceSpy = TestBed.inject(StoreCartService) as jasmine.SpyObj<StoreCartService>;
     catalogServiceSpy = TestBed.inject(StoreCatalogService) as jasmine.SpyObj<StoreCatalogService>;
+    searchServiceSpy.paginate.and.returnValue(EMPTY);
+    searchServiceSpy.reload.and.returnValue(EMPTY);
     searchServiceSpy.isLoadingSearch$ = of(false),
     searchServiceSpy.currentPage$ = of({
       items: [],
@@ -78,5 +87,32 @@ describe('StoreSearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load data right away', () => {
+    expect(searchServiceSpy.reload).toHaveBeenCalled();
+  });
+
+  it('should paginate data', () => {
+    const ev: PageEvent = { pageIndex: 1, pageSize: 5, length: 10 };
+    component.onPage(ev);
+    expect(searchServiceSpy.paginate).toHaveBeenCalled();
+  });
+
+  it('should add products to cart', () => {
+    const mockProduct = MOCK_PRODUCTS[Math.floor(Math.random() * MOCK_PRODUCTS.length)];
+    component.onAddProductToCart(mockProduct);
+    expect(cartServiceSpy.addProductToCart).toHaveBeenCalled();
+  });
+
+  it('should display details of products', () => {
+    const mockProduct = MOCK_PRODUCTS[Math.floor(Math.random() * MOCK_PRODUCTS.length)];
+    component.onViewProduct(mockProduct);
+    expect(catalogServiceSpy.navigateToProductDetails).toHaveBeenCalled();
+  });
+
+  it('should fail to display non-products', () => {
+    expect(() => component.onViewProduct(null)).toThrowError();
+    expect(catalogServiceSpy.navigateToProductDetails).not.toHaveBeenCalled();
   });
 });
