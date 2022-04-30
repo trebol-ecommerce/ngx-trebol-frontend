dfa97d2f44e7e366077393592976eeca70b0dd25
Benjamin La Madrid <bg.lamadrid@gmail.com>
2022-04-29 21:55:32 -0400

    test: improve coverage for product details dialog

--- src/app/store/dialogs/product-details/store-product-details-dialog.component.spec.ts
+++ src/app/store/dialogs/product-details/store-product-details-dialog.component.spec.ts
@@ -12,6 +12,7 @@ import { MatDialogModule, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { of } from 'rxjs';
+import { MOCK_PRODUCTS } from 'src/app/api/local-memory/mock/mock-products.datasource';
 import { Product } from 'src/models/entities/Product';
 import { StoreCartService } from '../../store-cart.service';
 import { StoreProductDetailsDialogComponent } from './store-product-details-dialog.component';
@@ -26,17 +27,11 @@ class MockSlideshowComponent {
 describe('StoreProductDetailsDialogComponent', () => {
   let component: StoreProductDetailsDialogComponent;
   let fixture: ComponentFixture<StoreProductDetailsDialogComponent>;
-  let cartService: Partial<StoreCartService>;
+  let cartServiceSpy: jasmine.SpyObj<StoreCartService>;
   let mockDialogData: Partial<StoreProductDetailsDialogData>;
 
   beforeEach(waitForAsync(() => {
-    // TODO use jasmine.SpyObj
-    cartService = {
-      cartDetails$: of([]),
-      increaseProductUnits(i) {},
-      addProductToCart(p) {},
-      decreaseProductUnits(i) {}
-    };
+    const mockCartService = jasmine.createSpyObj('StoreCartService', ['increaseProductUnits', 'addProductToCart', 'decreaseProductUnits']);
     mockDialogData = {
       product: new Product()
     };
@@ -54,19 +49,57 @@ describe('StoreProductDetailsDialogComponent', () => {
       ],
       providers: [
         { provide: MAT_DIALOG_DATA, useValue: mockDialogData },
-        { provide: StoreCartService, useValue: cartService }
+        { provide: StoreCartService, useValue: mockCartService }
       ]
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
+    cartServiceSpy = TestBed.inject(StoreCartService) as jasmine.SpyObj<StoreCartService>;
+    cartServiceSpy.cartDetails$ = of([]);
+
     fixture = TestBed.createComponent(StoreProductDetailsDialogComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('when the product is not in the cart', () => {
+    beforeEach(() => {
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should add products to the cart when not added yet', () => {
+      component.onClickIncreaseProductQuantity();
+      expect(cartServiceSpy.addProductToCart).toHaveBeenCalled();
+    });
+  });
+
+  describe('when the product is already in the cart', () => {
+    beforeEach(() => {
+      cartServiceSpy.cartDetails$ = of([{
+        product: MOCK_PRODUCTS[0],
+        units: 1,
+        unitValue: MOCK_PRODUCTS[0].price
+      }]);
+      mockDialogData.product = MOCK_PRODUCTS[0];
+      fixture.detectChanges();
+    });
+
+    it('should create', () => {
+      expect(component).toBeTruthy();
+    });
+
+    it('should increase the amount of units of that product in the cart', () => {
+      component.onClickIncreaseProductQuantity();
+      expect(cartServiceSpy.increaseProductUnits).toHaveBeenCalled();
+    });
+
+    it('should decrease the amount of units of that product in the cart', () => {
+      component.onClickDecreaseProductQuantity();
+      expect(cartServiceSpy.decreaseProductUnits).toHaveBeenCalled();
+    });
   });
 });
